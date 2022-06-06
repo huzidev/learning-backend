2f41e6a704fd7e8bd93fdc1df0ae0d0057c9e0f2
huzidev <huzaifa.iqdev@gmail.com>
2022-06-06 13:24:35 +0500

    query string

--- express/rest/index.js
+++ express/rest/index.js
@@ -5,11 +5,11 @@ const db = require('./db');
 const server = express(); // will create express application OR starts ours server
 
 server.get('/', (req, res) => {
-    res.send({message: 'hello world'})
+    res.send({message: 'hello world'});
 })
 
 server.get('/products', (req, res) => {
-    res.send({ list: db.getProducts() })
+    res.send({ list: db.getProducts() });
 })
 
 server.get('/product/:productId', (req, res) => {
@@ -18,21 +18,21 @@ server.get('/product/:productId', (req, res) => {
     const product = db.getProducts().find(((product) => product.id === parseInt(productId) ));
     // necessary to use parseINT to convert stringify int to simple int
     if (!product) {
-        res.status(404).send({ message: 'product not found'})
+        res.status(404).send({ message: 'product not found'});
         return;
     }
-    res.send({ product })
+    res.send({ product });
 })
 
 server.get('/products/search', (req, res) => { // ? means start of query string (means) specific item's info
     const { name } = req.query;
     console.log(name, 'name');
-    const products = db.getProducts().filter(((product) => product.name.includes(name) ))
+    const products = db.getProducts().filter(((product) => product.name.includes(name) ));
     if (!products.length) {
-        res.status(404).send({ message: `No product found with name ${name}`})
+        res.status(404).send({ message: `No product found with name ${name}`});
         return;
     }
-    res.send({ products })
+    res.send({ products });
 })
 
 server.listen(8000, () => {
