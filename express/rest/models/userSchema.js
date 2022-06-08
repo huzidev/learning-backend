a5f0ee36f693dbfcd1039a05f94acee578106d00
huzidev <huzaifa.iqdev@gmail.com>
2022-06-08 18:53:28 +0500

    creating user schema

--- express/rest/models/userSchema.js
+++ express/rest/models/userSchema.js
@@ -1,1 +1,8 @@
 const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({ // new for creating new instance
+    name : {
+        type : String,
+        required : true // means compulsory
+    },
+})
