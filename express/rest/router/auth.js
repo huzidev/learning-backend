d5c8ffef0ad2fe720b80fc5acf193bef9cacdd3f
huzidev <huzaifa.iqdev@gmail.com>
2022-06-08 22:32:20 +0500

    post method

--- express/rest/router/auth.js
+++ express/rest/router/auth.js
@@ -6,4 +6,9 @@ router.get('/', (req, res) => {
     res.send({ message: 'hello world Router.js' });
 });
 
+// when we wanted to GET user data we uses POST because GET just brings data and POST Reads it or collect it so we can read it
+router.post('/register', (req, res) => {
+    console.log(res.body);
+})
+
 module.exports = router;
