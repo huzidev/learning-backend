1b3a8791643a0e71bb2a65bbeb3f21a1de98e379
huzidev <huzaifa.iqdev@gmail.com>
2022-06-08 18:59:49 +0500

    models

--- express/rest/models/userSchema.js
+++ express/rest/models/userSchema.js
@@ -7,14 +7,17 @@ const userSchema = new mongoose.Schema({ // new for creating new instance(exampl
     },
     email : {
         type : String,
-        required : true // means compulsory
+        required : true
     },
     phone : {
         type : Number,
-        required : true // means compulsory
+        required : true
     },
     name : {
         type : String,
-        required : true // means compulsory
+        required : true
     },
 })
+
+// model for linking ours schema with collection at mongoDB
+const User = mongoose.model('USER', userSchema)
