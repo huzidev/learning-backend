657adc62830a0b55dbff60c06b7aefac20841a81
huzidev <huzaifa.iqdev@gmail.com>
2022-06-08 19:01:23 +0500

    mongodb collection

--- express/rest/models/userSchema.js
+++ express/rest/models/userSchema.js
@@ -20,4 +20,7 @@ const userSchema = new mongoose.Schema({ // new for creating new instance(exampl
 })
 
 // model for linking ours schema with collection at mongoDB
-const User = mongoose.model('USER', userSchema)
+// const User is variable name and first word should've to be capital
+const User = mongoose.model('USER', userSchema); 
+// USER Parameter is the name of collection going to be created at DOCUMENT
+// userScheme is the way we wanted ours user data to be stored
