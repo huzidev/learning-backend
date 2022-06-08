ec51bd82f3c8cc392aef03256716bfd156d5456d
huzidev <huzaifa.iqdev@gmail.com>
2022-06-08 18:54:25 +0500

    schema models

--- express/rest/models/userSchema.js
+++ express/rest/models/userSchema.js
@@ -5,4 +5,16 @@ const userSchema = new mongoose.Schema({ // new for creating new instance
         type : String,
         required : true // means compulsory
     },
+    email : {
+        type : String,
+        required : true // means compulsory
+    },
+    phone : {
+        type : Number,
+        required : true // means compulsory
+    },
+    name : {
+        type : String,
+        required : true // means compulsory
+    },
 })
