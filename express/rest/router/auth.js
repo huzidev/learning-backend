537e0511014754a11f2b232e120a4d401f27393a
huzidev <huzaifa.iqdev@gmail.com>
2022-06-09 18:57:10 +0500

    async await fucntion

--- express/rest/router/auth.js
+++ express/rest/router/auth.js
@@ -14,25 +14,34 @@ router.post('/register', async (req, res) => {
         return res.status(422).json({ error : "You've left an tag empty" }); // because data is going to be in JSON format
     }
 
-    // to check if user is already registered LEFT ONE IS KEY AND RIGHT ONE IS PROPERTY
-    User.findOne({ email : email }) // email at left is for userSchema and email at right is the email user going to insert
-    // this email : email means if email at database is equal to email user is going to insert means promise TRUE hence .then
-        .then((userExist) => {
-            if (userExist) {
-                return res.status(422).json({ error : "Email Already Exist" })
-            }
-            //if user not REGISTERED already then we'll create an NEW Document for user data therefore we've used (const user = new User)
-            const user = new User({ username, email, number, password, cpassword });//IF the instance is like username : name then we've 
-            // to write it like that Example username : name etc means if key and property are same then just write any one
 
-            user.save().then(() => {
-                res.status(201).json({ message : "User Registered Successfully!" });
-            }).catch((err) => {
-                res.status(500).json({ error : "Failed To Registered" }); // 500 is database error
-            })
-        }).catch((err) => {
-            console.log(err);
-        });
+    try{
+        // to check if user is already registered LEFT ONE IS KEY AND RIGHT ONE IS PROPERTY
+        const userExist = await User.findOne({ email : email });// email at left is for userSchema and email at right is the email user going to insert
+        // this email : email means if email at database is equal to email user is going to insert means promise TRUE hence .then
+
+        if (userExist) {
+            return res.status(422).json({ error : "Email Already Exist" });
+        }
+        
+        //if user not REGISTERED already then we'll create an NEW Document for user data therefore we've used (const user = new User)
+        const user = new User({ username, email, number, password, cpassword });//IF the instance is like username : name then we've 
+        // to write it like that Example username : name etc means if key and property are same then just write any one
+
+        const userRegister = await user.save();
+
+        if (userRegister) {
+            res.status(201).json({ message : "User Registered Successfully!" });
+        }
+        else{
+            res.status(500).json({ error : "Failed To Registered" }); // 500 is database error
+        }
+    }
+    
+    catch{
+        console.log(err);
+    }
+        
 });
 
 module.exports = router;
