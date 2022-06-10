b9d618fb2ee5987495abecf6580197a38e73db90
huzidev <huzaifa.iqdev@gmail.com>
2022-06-10 22:56:52 +0500

    response method

--- express/rest/router/auth.js
+++ express/rest/router/auth.js
@@ -103,8 +103,35 @@ router.post('/login', async(req, res) => {
     }
 })
 
-router.post('/messge', async (req, res) => {
+router.post('/message', async (req, res) => {
+
+    const { username, email, number, message } = req.body;
+
+    if ( !username || !email || !number || !message ) {
+        return res.status(422).json({ error : "You've left an tag empty" }); // because data is going to be in JSON format
+    }
+
+    try{
+
+            //if user not REGISTERED already then we'll create an NEW Document for user data therefore we've used (const user = new User)
+            const user = new User({ username, email, number, message });//IF the instance is like username : name then we've 
+            // to write it like that Example username : name etc means if key and property are same then just write any one
     
+            const userRegister = await user.save();
+    
+            if (userRegister) {
+                res.status(201).json({ message : "User Registered Successfully!" });
+            }
+            else{
+                res.status(500).json({ error : "Failed To Registered" }); // 500 is database error
+            }
+        
+    }
+
+    catch (err) {
+        console.log(err);
+    }
+
 })
 
 module.exports = router;
