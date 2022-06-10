1fde23ca26b714c5bc1fee6cd8887e96e44057c3
huzidev <huzaifa.iqdev@gmail.com>
2022-06-10 22:46:30 +0500

    minimum lenght added for message

--- express/rest/models/userSchema.js
+++ express/rest/models/userSchema.js
@@ -23,6 +23,12 @@ const userSchema = new mongoose.Schema({ // new for creating new instance(exampl
         type : String,
         required : true
     },
+
+    message : {
+        type : String,
+        required : true,
+        minlength : 150
+    }
     // for storing the token for particular email where _id matches therefore we've write {_id : this_id}
     tokens : [
         {
