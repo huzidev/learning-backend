df8ff8b3eda3c0e024e1d97b61faa742243f729c
huzidev <huzaifa.iqdev@gmail.com>
2022-06-10 22:46:44 +0500

    message portion

--- express/rest/models/userSchema.js
+++ express/rest/models/userSchema.js
@@ -27,7 +27,7 @@ const userSchema = new mongoose.Schema({ // new for creating new instance(exampl
     message : {
         type : String,
         required : true,
-        minlength : 150
+        minlength : 100
     }
     // for storing the token for particular email where _id matches therefore we've write {_id : this_id}
     tokens : [
