7b412ef02f15a8161b6e064e141d5f2364392bff
huzidev <huzaifa.iqdev@gmail.com>
2022-06-13 13:24:16 +0500

    secret key for token

--- express/rest/middleware/token.js
+++ express/rest/middleware/token.js
@@ -4,6 +4,9 @@ const jwt = require('jsonwebtoken');
 const token = async (req, res, next) => {
     try{
 
+        const userToken = req.cookies.jwt;
+        const verifyUser = jwt.verify(userToken, process.env.SECRET_KEY);
+        
     }
     catch (err) {
         res.status(401).send(err);
