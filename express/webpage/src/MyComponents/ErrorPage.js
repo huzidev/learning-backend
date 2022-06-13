24f9df81801c89bab6290bb2e1bb8733542ebdf2
huzidev <huzaifa.iqdev@gmail.com>
2022-06-13 21:58:13 +0500

    error page info

--- express/webpage/src/MyComponents/ErrorPage.js
+++ express/webpage/src/MyComponents/ErrorPage.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
 
 export default function ErrorPage() {
     return (
         <div>
-            
+            <h1>
+                404
+            </h1>
+            <h2>
+                We are sorry, page not found!
+            </h2>
         </div>
     )
 }
