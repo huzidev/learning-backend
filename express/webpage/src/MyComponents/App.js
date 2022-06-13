a3bcfca32a1778ab1ec38816c66169205307b91d
huzidev <huzaifa.iqdev@gmail.com>
2022-06-13 22:09:37 +0500

    app.js

--- express/webpage/src/MyComponents/App.js
+++ express/webpage/src/MyComponents/App.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import About from './About';
+import ContactUs from './ContactUs';
+import login from './login';
+import Register from './Register';
+import ErrorPage from './ErrorPage';
 
 export default function App() {
     return (
