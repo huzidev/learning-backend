f68bc0fc3a22637cb0a3a33af8d10c553095cbc8
huzidev <huzaifa.iqdev@gmail.com>
2022-06-13 13:22:20 +0500

    try catch for error

--- express/rest/middleware/token.js
+++ express/rest/middleware/token.js
@@ -1,2 +1,11 @@
 const User = require('../models/userSchema');
 const jwt = require('jsonwebtoken');
+
+const token = async (req, res, next) => {
+    try{
+
+    }
+    catch (err) {
+        res.status(401).send(err);
+    }
+}
