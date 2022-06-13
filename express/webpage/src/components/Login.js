60eef4978006fef5546ea7a04b5de37b301468d2
huzidev <huzaifa.iqdev@gmail.com>
2022-06-13 17:57:29 +0500

    login page

--- express/webpage/src/components/Login.js
+++ express/webpage/src/components/Login.js
@@ -7,7 +7,11 @@ export default function Login() {
 
     return (
         <div>
-
+            <form action="/secret" method="post">
+                <input type="email" name="email" placeholder="email" />
+                <input type="password" name="password" placeholder="password" />
+                <input type="submit" value="send" />
+            </form>
         </div>
     )
 }
