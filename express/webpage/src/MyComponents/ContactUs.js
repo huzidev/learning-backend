518d3c78f38e31738053145854acb19ab57bb988
huzidev <huzaifa.iqdev@gmail.com>
2022-06-13 21:43:20 +0500

    request method post

--- express/webpage/src/MyComponents/ContactUs.js
+++ express/webpage/src/MyComponents/ContactUs.js
@@ -1,7 +1,51 @@
-import React from 'react'
+import React from 'react';
 
 export default function ContactUs() {
     return (
-        <div></div>
+        <div>
+            <form method="POST">
+                <input 
+                    type="text" 
+                    name="username" 
+                    value={email} 
+                    placeholder="username" 
+                    onChange={(event) => setEmail(event.target.value)} // so we can get what user is typing
+                />
+                <input 
+                    type="text" 
+                    name="username" 
+                    value={email} 
+                    placeholder="email" 
+                    onChange={(event) => setEmail(event.target.value)} // so we can get what user is typing
+                />
+                <input 
+                    type="email" 
+                    name="number" 
+                    value={email} 
+                    placeholder="contact number" 
+                    onChange={(event) => setEmail(event.target.value)} // so we can get what user is typing
+                />
+                <input 
+                    type="password" 
+                    name="password" 
+                    value={password} 
+                    placeholder="password" 
+                    onChange={(event) => setPassword(event.target.value)} 
+                />
+                <input 
+                    type="password" 
+                    name="cpassword" 
+                    value={password} 
+                    placeholder="confirm password" 
+                    onChange={(event) => setPassword(event.target.value)} 
+                />
+                <input 
+                    type="submit"
+                    name="login"
+                    value="log in"
+                    onClick={loginUser}
+                 />
+            </form>
+        </div>
     )
 }
