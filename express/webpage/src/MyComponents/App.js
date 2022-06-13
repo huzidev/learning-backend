b9cea393566745d86c8c9e0512e8eb703c7d0b4c
huzidev <huzaifa.iqdev@gmail.com>
2022-06-13 22:11:39 +0500

    home page created

--- express/webpage/src/MyComponents/App.js
+++ express/webpage/src/MyComponents/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import About from './About';
 import ContactUs from './ContactUs';
-import login from './login';
+import Login from './Login';
 import Register from './Register';
 import ErrorPage from './ErrorPage';
 
 export default function App() {
     return (
         <div>
-            
+            <Route exact path="/">
+                
+            </Route>
         </div>
     )
 }
