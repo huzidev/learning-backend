405f917c1eda276a6b231461c8bea257c387c32a
huzidev <huzaifa.iqdev@gmail.com>
2022-06-13 21:33:25 +0500

    boiler plate for regiter page

--- express/webpage/src/MyComponents/Register.js
+++ express/webpage/src/MyComponents/Register.js
@@ -3,7 +3,50 @@ import React from 'react';
 export default function register() {
     return (
         <div>
-
+            <form method="POST"> 
+            {/* didn't need to use action method here */}
+                <input 
+                    type="text" 
+                    name="username" 
+                    value={email} 
+                    placeholder="email" 
+                    onChange={(event) => setEmail(event.target.value)} // so we can get what user is typing
+                />
+                <input 
+                    type="text" 
+                    name="username" 
+                    value={email} 
+                    placeholder="email" 
+                    onChange={(event) => setEmail(event.target.value)} // so we can get what user is typing
+                />
+                <input 
+                    type="email" 
+                    name="number" 
+                    value={email} 
+                    placeholder="email" 
+                    onChange={(event) => setEmail(event.target.value)} // so we can get what user is typing
+                />
+                <input 
+                    type="password" 
+                    name="password" 
+                    value={password} 
+                    placeholder="password" 
+                    onChange={(event) => setPassword(event.target.value)} 
+                />
+                <input 
+                    type="password" 
+                    name="cpassword" 
+                    value={password} 
+                    placeholder="password" 
+                    onChange={(event) => setPassword(event.target.value)} 
+                />
+                <input 
+                    type="submit"
+                    name="login"
+                    value="log in"
+                    onClick={loginUser}
+                 />
+            </form>
         </div>
     )
 }
