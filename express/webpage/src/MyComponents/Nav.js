749f2617fbdc4e0c0230a68d34ebc3084ee32bc0
huzidev <huzaifa.iqdev@gmail.com>
2022-06-13 22:29:03 +0500

    nav bar created

--- express/webpage/src/MyComponents/Nav.js
+++ express/webpage/src/MyComponents/Nav.js
@@ -3,7 +3,11 @@ import React from 'react';
 export default function Nav() {
     return (
         <div>
-            
+            <a href="#">Home</a>
+            <a href="#">About</a>
+            <a href="#">Contact Us</a>
+            <a href="#">Login</a>
+            <a href="#">Register</a>
         </div>
     )
 }
