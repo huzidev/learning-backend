8bb58505a868b4025dd1e84c4d4bf059cc80b444
huzidev <huzaifa.iqdev@gmail.com>
2022-06-14 21:39:01 +0500

    errors fixedf

--- express/webpage/client/src/MyComponents/Register.js
+++ express/webpage/client/src/MyComponents/Register.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
-export default function register() {
+export default function Register() {
 
     const history = useNavigate();
 
