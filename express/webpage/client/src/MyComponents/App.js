30d82263bc90b465e0d88d353cd71cf0d2850dc8
huzidev <huzaifa.iqdev@gmail.com>
2022-06-14 21:28:53 +0500

    pkg updated

--- express/webpage/client/src/MyComponents/App.js
+++ express/webpage/client/src/MyComponents/App.js
@@ -6,34 +6,38 @@ import ContactUs from './ContactUs';
 import Login from './Login';
 import Register from './Register';
 import ErrorPage from './ErrorPage';
-import { Route, Switch } from 'react-router-dom';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+  } from "react-router-dom";
 
 export default function App() {
     return (
         <div>
-
             <Nav />
-
-            <Switch >
-                <Route exact path="/">
-                    <Home />
-                </Route>
-                <Route exact path="/about">
-                    <About />
-                </Route>
-                <Route exact path="/contact">
-                    <ContactUs />
-                </Route>
-                <Route exact path="/login">
-                    <Login />
-                </Route>
-                <Route exact path="/register">
-                    <Register />
-                </Route>
-                <Route>
-                    <ErrorPage />
-                </Route>
-            </Switch>
+            <Router>
+                <Routes>
+                    <Route exact path="/">
+                        <Home />
+                    </Route>
+                    <Route exact path="/about">
+                        <About />
+                    </Route>
+                    <Route exact path="/contact">
+                        <ContactUs />
+                    </Route>
+                    <Route exact path="/login">
+                        <Login />
+                    </Route>
+                    <Route exact path="/register">
+                        <Register />
+                    </Route>
+                    <Route>
+                        <ErrorPage />
+                    </Route>
+                </Routes>
+            </Router>
         </div>
     )
 }
