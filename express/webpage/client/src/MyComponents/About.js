4ab235c3723be88bd5f486f8712a1b6f94013bfa
huzidev <huzaifa.iqdev@gmail.com>
2022-06-16 14:36:11 +0500

    async function

--- express/webpage/client/src/MyComponents/About.js
+++ express/webpage/client/src/MyComponents/About.js
@@ -2,8 +2,18 @@ import React from 'react';
 
 export default function About() {
 
-    React.useEffect(() => {
+    const CallAboutPage = async () => {
+        try{
+            const res = await fetch('/about')
+        }
 
+        catch (err) {
+            console.log(err);
+        }
+    }
+
+    React.useEffect(() => {
+        CallAboutPage();
     }, []);
 
     return (
