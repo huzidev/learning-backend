e9f50dfa7e06e3fdaed600b437367a209d5f22c9
huzidev <huzaifa.iqdev@gmail.com>
2022-06-16 13:36:33 +0500

    try catch

--- express/webpage/server/middleware/Verification.js
+++ express/webpage/server/middleware/Verification.js
@@ -3,6 +3,13 @@ const User = require('../models/userSchema');
 
 const Verification = async (req, res, next) => {
 
+    try{
+
+    }
+    catch (err) {
+        console.log(err);
+    }
+
 }
 
 module.exports = Verification;
