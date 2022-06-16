313a7ff55bd49e2d055fe91aa383a1aeef32f96d
huzidev <huzaifa.iqdev@gmail.com>
2022-06-16 12:33:09 +0500

    JSX address to

--- express/webpage/client/src/MyComponents/Nav.js
+++ express/webpage/client/src/MyComponents/Nav.js
@@ -4,11 +4,11 @@ import { Link } from 'react-router-dom';
 export default function Nav() {
     return (
         <div>
-            <Link href="/">Home</Link>
-            <Link href="/about">About</Link>
-            <Link href="/contact">Contact Us</Link>
-            <Link href="/login">Login</Link>
-            <Link href="/register">Register</Link>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/contact">Contact Us</Link>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
         </div>
     )
 }
