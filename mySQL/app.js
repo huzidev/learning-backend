ec01bba396d09cf00e8ae14932661ae15e17ea18
huzidev <huzaifa.iqdev@gmail.com>
2022-06-18 16:24:36 +0500

    home page for server

--- mySQL/app.js
+++ mySQL/app.js
@@ -3,6 +3,10 @@ import express from "express";
 const server = express();
 const port = 8000;
 
+server.get('/', (req, res) => {
+    res.send('Home Page');
+});
+
 server.listen(port, (err) => {
     if (!err) {
         console.log(`Server listening on port ${port}`);
