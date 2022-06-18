617e05473cd04e2afbf0e7cefb80e735d9c66d25
huzidev <huzaifa.iqdev@gmail.com>
2022-06-18 15:39:34 +0500

    if else conditions

--- mySQL/app.js
+++ mySQL/app.js
@@ -3,6 +3,11 @@ import express from "express";
 const server = express();
 const port = 8000;
 
-server.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
+server.listen(port, (err) => {
+    if (!err) {
+        console.log(`Server listening on port ${port}`);
+    }
+    else{
+        console.log(err);
+    }
 })
