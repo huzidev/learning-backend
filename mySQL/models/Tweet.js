bfbbc054b7d280b3329caf08b8e58c19293dee01
huzidev <huzaifa.iqdev@gmail.com>
2022-06-18 19:14:47 +0500

    mysql table

---
+++ mySQL/models/Tweet.js
@@ -0,0 +1,9 @@
+import { Sequelize } from "sequelize";
+
+module.exports = sequelize.define("Tweets", { // will take the name of model we wanted to create Tweet is basically a table(document)
+
+//all the schema attribute inside their is going to represent  column to this table tweet
+
+
+
+});
