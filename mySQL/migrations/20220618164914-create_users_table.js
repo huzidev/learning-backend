e28f260719255be5f6b8d7d8355b848d6cb0695c
huzidev <huzaifa.iqdev@gmail.com>
2022-06-18 21:51:36 +0500

    up and down for migration

--- mySQL/migrations/20220618164914-create_users_table.js
+++ mySQL/migrations/20220618164914-create_users_table.js
@@ -2,10 +2,26 @@
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    
+    return queryInterface.createTable("users", {
+      id : {
+        type : Sequelize.INTEGER(11),
+        allowNull : false,
+        autoIncrement : true,
+        primaryKey : true
+      },
+      username : {
+          type : Sequelize.STRING(25),
+          allowNull : false,
+          unique : true, // so each username must be unique not same
+      },
+      password : {
+          type : Sequelize.STRING(20),
+          allowNull : false,
+      }
+    })
   },
 
   async down (queryInterface, Sequelize) {
-    
+    return queryInterface.dropTable("users");
   }
 };
