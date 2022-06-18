5a05aabf04a37389e80b9a9f23af393585390fd2
huzidev <huzaifa.iqdev@gmail.com>
2022-06-18 16:31:30 +0500

    authenticate for database

--- mySQL/models/index.js
+++ mySQL/models/index.js
@@ -3,5 +3,13 @@ import { Sequelize } from "sequelize";
 const sequelize = new Sequelize('user', 'root', '', {
     host : "127.0.0.1",
     dialect : "mysql",
-    
+    pool : {
+        max : 5,
+        min : 0,
+        idle : 10000 // idle time req for going from one connection to another in ms
+    }
+});
+
+sequelize.authenticate().then(() => {
+    console.log("Connected to database");
 })
