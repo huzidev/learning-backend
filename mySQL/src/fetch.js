1be37defb32d46d4c512a99a6dfc60f47fad1494
huzidev <huzaifa.iqdev@gmail.com>
2022-06-18 22:28:18 +0500

    errorHandler

--- mySQL/src/fetch.js
+++ mySQL/src/fetch.js
@@ -2,9 +2,13 @@ import Tweet from './models/Tweet';
 import User from './models/User';
 module.exports = async () => {
 
+    const errorHandler = (err) => {
+        console.log("Error : " + err);
+    }
+
     User.create({
         username : "huzi",
         password : "test123"
-    })
+    }).catch()
 
 }
