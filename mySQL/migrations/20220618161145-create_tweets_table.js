733b8e4e4ba81558eaa4882fe91b872f90044bf4
huzidev <huzaifa.iqdev@gmail.com>
2022-06-18 21:32:08 +0500

    attributes

--- mySQL/migrations/20220618161145-create_tweets_table.js
+++ mySQL/migrations/20220618161145-create_tweets_table.js
@@ -7,7 +7,19 @@
 //queryInterface allows us to create tables, columns and interacting with database
 module.exports = {
   async up (queryInterface, Sequelize) {
-    queryInterface.  
+    queryInterface.createTable("tweets", { // tweets is ours table name and second thing in object is ours actual schema
+      id : {
+        type : Sequelize.INTEGER(11),
+        allowNull : false, // means required true
+        autoIncrement : true,
+        primaryKey : true
+      },
+      content : Sequelize.STRING(300),
+      //createAt creates date and time inside of that attribute 
+      createdAt : Sequelize.DATE,
+      updatedAt : Sequelize.DATE,
+      //updateAt will update date and time if we wanted to update anything
+    })  
   },
 
   async down (queryInterface, Sequelize) {
