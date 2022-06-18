b89da2660ffe7338a33f4371ac13f706b101c3bd
huzidev <huzaifa.iqdev@gmail.com>
2022-06-18 22:41:36 +0500

    associtaion completetd

--- mySQL/src/fetch.js
+++ mySQL/src/fetch.js
@@ -5,6 +5,10 @@ import User from './models/User';
 // therefore we've to create an ASSOCIATION here
 module.exports = async () => {
 
+// this ASSOCIATION helps us to grab particular stuff we wanted ex if we wanted to extract specific MANY TWEETS FROM SINGLE USER WE
+// can because of this association like wise we can extract specific tweet from specific user OR all the users who have tweets the same
+// tweet because of this association 
+
     User.hasMany(Tweet, {
         as : "Tweets",
         foreignKey : "userId"
