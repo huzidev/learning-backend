f185c712f47d56e617ce3ba639ce5a18d74efa3f
huzidev <huzaifa.iqdev@gmail.com>
2022-06-18 22:27:22 +0500

    fetch method

--- mySQL/src/fetch.js
+++ mySQL/src/fetch.js
@@ -2,6 +2,9 @@ import Tweet from './models/Tweet';
 import User from './models/User';
 module.exports = async () => {
 
-    
+    User.create({
+        username : "huzi",
+        password : "test123"
+    })
 
 }
