c5e15d1fa193209b3bce1b8217d3e3a7f54511e1
huzidev <huzaifa.iqdev@gmail.com>
2022-06-19 19:21:52 +0500

    force method

--- mySQL/FullStack-Web/models/index.js
+++ mySQL/FullStack-Web/models/index.js
@@ -33,3 +33,6 @@ db.sequelize = sequelize
 
 db.products = require('./productModel.js')(sequelize, DataTypes)
 db.reviews = require('./reviewModel.js')(sequelize, DataTypes)
+
+db.sequelize.sync({ force : false })// if we didn't do this than every time we run the server the force will overwrite all the 
+// data present in the table with new data and we don't want that until we really wanted it therefore it is necessary to use force : false
