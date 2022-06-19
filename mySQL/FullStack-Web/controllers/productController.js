dd204e2ead9f97f9e56d8b30345411f848a781cf
huzidev <huzaifa.iqdev@gmail.com>
2022-06-19 20:01:05 +0500

    req body

--- mySQL/FullStack-Web/controllers/productController.js
+++ mySQL/FullStack-Web/controllers/productController.js
@@ -1,3 +1,18 @@
 import db from '../models'
 
 // CREATING MAIN MODEL for passing our data and get ours data from database
+const Product = db.products
+const Review = db.reviews
+
+// main work
+
+// create product
+
+const addProduct = async (req, res) => {
+    let info = {
+        title : req.body.title,
+        price : req.body.price,
+        description : req.body.description,
+        published : req.body.published ? req.body.published : false
+    } 
+}
