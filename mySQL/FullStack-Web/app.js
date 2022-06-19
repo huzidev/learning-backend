fd164ec4e08c487cd3c3b0198edbd47340a1f244
huzidev <huzaifa.iqdev@gmail.com>
2022-06-19 18:12:04 +0500

    json format

--- mySQL/FullStack-Web/app.js
+++ mySQL/FullStack-Web/app.js
@@ -1,6 +1,15 @@
 import express from "express";
+import cors from "cors";
+
 const server = express();
 const port = 8000;
+
+var corOptions = {
+    origin : '127.0.0.1:8000'
+}
+
+server.use(cors(corOptions));
+server.use(express.json());
 
 // import bodyParser from "body-parser";
 
