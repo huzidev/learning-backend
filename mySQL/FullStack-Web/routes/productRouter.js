8f7e42597c0fc9771f0b454dd61edd778244d990
huzidev <huzaifa.iqdev@gmail.com>
2022-06-19 21:59:41 +0500

    routers added

--- mySQL/FullStack-Web/routes/productRouter.js
+++ mySQL/FullStack-Web/routes/productRouter.js
@@ -3,3 +3,15 @@ import productController from '../controllers/productController'
 const router = require('express').Router()
 
 router.post('/addProduct', productController.addProduct)
+
+
+router.get('/allProducts', productController.getAllProducts)
+
+router.get('/published', productController.getPublishedProduct)
+
+
+router.get('/:id', productController.getOneProduct)
+
+router.put('/:id', productController.updateProduct)
+
+router.delete('/:id', productController.deleteProduct)
