4d9a298393470e688226af1b0e6b82858b7102d6
huzidev <huzaifa.iqdev@gmail.com>
2022-06-19 18:03:18 +0500

    initial commit

---
+++ mySQL/FullStack-Web/app.js
@@ -0,0 +1,27 @@
+import express from "express";
+const server = express();
+const port = 8000;
+
+// import bodyParser from "body-parser";
+
+// server.use(bodyParser.json());
+// server.use(bodyParser.urlencoded({extended : true}))
+// const router = express.Router();
+// server.use(router);
+
+
+require ('./src/database/index');// import sequelize
+require ('./src/fetch')(); // for executing it we've to put function at the end
+
+server.get('/', (req, res) => {
+    res.send('Home Page');
+});
+
+server.listen(port, (err) => {
+    if (!err) {
+        console.log(`Server listening on port ${port}`);
+    }
+    else{
+        console.log(err);
+    }
+});
