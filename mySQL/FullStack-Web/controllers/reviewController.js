4fd77d7b818b3647243e3d7c4d83eb86ec778e2b
huzidev <huzaifa.iqdev@gmail.com>
2022-06-20 21:31:30 +0500

    linking models

--- mySQL/FullStack-Web/controllers/reviewController.js
+++ mySQL/FullStack-Web/controllers/reviewController.js
@@ -1,3 +1,15 @@
 import db from '../models'
 
 const Review = db.reviews
+
+// FUNCTIONS
+
+// Add Review
+
+const addReview = async (req, res) => {
+
+    let data = {
+        rating : req.body.rating // rating is been defined in reviewModels
+    }
+
+}
