0cf6580bbad887a90bc5f52dfb84d33b19af7be0
huzidev <huzaifa.iqdev@gmail.com>
2022-06-20 21:46:20 +0500

    review routers fix

--- mySQL/FullStack-Web/app.js
+++ mySQL/FullStack-Web/app.js
@@ -17,7 +17,8 @@ server.use(express.urlencoded({extended : true}))
 //routers
 import routerProduct from './routes/productRouter'
 import routerReview from './routes/reviewRouter'
-server.use('/api/products', router) // /api/products will just get at the start then we can go to any route we wanted 
+server.use('/api/products', routerProduct) // /api/products will just get at the start then we can go to any route we wanted 
+server.use('/api/reviews', routerReview)
 
 server.get('/', (req, res) => {
     res.json({ message : "Hello From API" });
