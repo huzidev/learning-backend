0f978441655625eb629ae955c2605c1d5f9284ae
huzidev <huzaifa.iqdev@gmail.com>
2022-06-21 18:53:14 +0500

    style components for pages

--- mySQL/FullStack-Web/client/src/MyComponents/ProductCard.js
+++ mySQL/FullStack-Web/client/src/MyComponents/ProductCard.js
@@ -3,7 +3,15 @@ import React from 'react';
 export default function ProductCard({ product }) {
   return (
     <div>
-
+        <h1>
+            Tittle : {product.title}
+        </h1>
+        <h2>
+            Prce : {product.price}
+        </h2>
+        <h3>
+            Description : {product.description}
+        </h3>
     </div>
   )
 }
