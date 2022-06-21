9291fb77d8da129a8b34b847f344199b27ec84e2
huzidev <huzaifa.iqdev@gmail.com>
2022-06-21 19:00:01 +0500

    updating product cards

--- mySQL/FullStack-Web/client/src/App.js
+++ mySQL/FullStack-Web/client/src/App.js
@@ -4,17 +4,17 @@ import {
     Routes,
     Route,
   } from "react-router-dom";
-import AddProducts from '../screens/AddProducts';
-import EditProduct from '../screens/EditProduct';
-import ShowProducts from '../screens/ShowProducts';
-import ProductDetails from '../screens/ProductDetails';
+import AddProducts from './screens/AddProducts';
+import EditProduct from './screens/EditProduct';
+import ShowProducts from './screens/ShowProducts';
+import ProductDetails from './screens/ProductDetails';
 
 export default function App() {
   return (
     <div>
         <Router >
             <Routes>
-                <Route exact path="/addProduct" element={AddProducts}/>
+                <Route exact path="/addProducts" element={AddProducts}/>
                 <Route exact path="/products" element={ShowProducts}/>
                 <Route exact path="/product/edit/:id" element={EditProduct}/>
                 <Route exact path="/product/:id" element={ProductDetails}/>
