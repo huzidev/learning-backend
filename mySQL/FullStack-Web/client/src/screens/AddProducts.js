6e243b20a5a4eb3292c111f70354e3fc5903f43e
huzidev <huzaifa.iqdev@gmail.com>
2022-06-21 19:35:48 +0500

    boolean logic

--- mySQL/FullStack-Web/client/src/screens/AddProducts.js
+++ mySQL/FullStack-Web/client/src/screens/AddProducts.js
@@ -1,6 +1,20 @@
 import React from 'react';
 
 export default function AddProducts() {
+
+    const [title, setTitle] = React.useState('');
+    const [price, setPrice] = React.useState(0);
+    const [description, setDescription] = React.useState('');
+
+    const addProductHandler = async () => {
+        const data = {
+            title : title,
+            price : price,
+            description : description,
+            published : true
+        }
+    }
+
   return (
     <div>
         <h1>
@@ -10,6 +24,7 @@ export default function AddProducts() {
             <input type="text" placeholder='product name'/>
             <input type="number" placeholder='price' />
             <input type="text" placeholder='description' />
+            <input type="submit" />
         </form>
     </div>
   )
