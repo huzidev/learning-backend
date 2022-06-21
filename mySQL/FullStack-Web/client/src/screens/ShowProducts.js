196d726b06ef8f776b53f71158b6e4a884bfb30b
huzidev <huzaifa.iqdev@gmail.com>
2022-06-21 18:44:58 +0500

    products mapping

--- mySQL/FullStack-Web/client/src/screens/ShowProducts.js
+++ mySQL/FullStack-Web/client/src/screens/ShowProducts.js
@@ -24,8 +24,8 @@ export default function ShowProducts() {
         {
             products.map((product) => (
                 <>
-                    <li>
-                        
+                    <li key={product.id}>
+                        {product.title}
                     </li>
                 </>
             )) 
