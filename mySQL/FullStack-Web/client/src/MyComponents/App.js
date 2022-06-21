4b25a64650fc73b2fbdbc7c320847a744d9c941b
huzidev <huzaifa.iqdev@gmail.com>
2022-06-21 18:32:37 +0500

    router links added

---
+++ mySQL/FullStack-Web/client/src/MyComponents/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+  } from "react-router-dom";
+import AddProducts from '../screens/AddProducts';
+import EditProduct from '../screens/EditProduct';
+import ShowProducts from '../screens/ShowProducts';
+
+export default function App() {
+  return (
+    <div>
+        <Router >
+            <Routes>
+                <Route exact path="/addProduct" element={AddProducts}/>
+                <Route exact path="/Products" element={ShowProducts}/>
+                <Route exact path="/Product/edit/:id" element={EditProduct}/>
+                <Route exact path="/Product/:id" element={ShowProducts}/>
+            </Routes>
+        </Router>
+    </div>
+  )
+}
--- mySQL/FullStack-Web/client/src/screens/ProductDetails.js
+++ mySQL/FullStack-Web/client/src/screens/ProductDetails.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import axios from 'axios';
 
 export default function ProductDetails() {
