458cb7666a580f6ee643f40fd7de3002bb6e0491
huzidev <huzaifa.iqdev@gmail.com>
2022-06-21 19:32:08 +0500

    form added

--- mySQL/FullStack-Web/client/src/MyComponents/ProductCard.js
+++ mySQL/FullStack-Web/client/src/MyComponents/ProductCard.js
@@ -4,16 +4,19 @@ import { Link } from 'react-router-dom';
 export default function ProductCard({ product }) {
   return (
     <div>
+        <h1>
+            Tittle : {product.title}
+        </h1>
+        <h2>
+            Prce : {product.price}
+        </h2>
+        <h3>
+            Description : {product.description}
+        </h3>
         <Link to={`product/${product.id}`}>
-            <h1>
-                Tittle : {product.title}
-            </h1>
-            <h2>
-                Prce : {product.price}
-            </h2>
-            <h3>
-                Description : {product.description}
-            </h3>
+            <button>
+                Details
+            </button>
         </Link>
     </div>
   )
--- mySQL/FullStack-Web/client/src/screens/AddProducts.js
+++ mySQL/FullStack-Web/client/src/screens/AddProducts.js
@@ -3,7 +3,14 @@ import React from 'react';
 export default function AddProducts() {
   return (
     <div>
-        
+        <h1>
+            Add Products
+        </h1>     
+        <form >
+            <input type="text" placeholder='product name'/>
+            <input type="number" placeholder='price' />
+            <input type="text" placeholder='description' />
+        </form>
     </div>
   )
 }
