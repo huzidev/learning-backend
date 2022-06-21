51f186ea37c3d9b8b18c8387c9e1d6c231ba2bff
huzidev <huzaifa.iqdev@gmail.com>
2022-06-21 19:01:15 +0500

    Link to

--- mySQL/FullStack-Web/client/src/MyComponents/ProductCard.js
+++ mySQL/FullStack-Web/client/src/MyComponents/ProductCard.js
@@ -1,17 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function ProductCard({ product }) {
   return (
     <div>
-        <h1>
-            Tittle : {product.title}
-        </h1>
-        <h2>
-            Prce : {product.price}
-        </h2>
-        <h3>
-            Description : {product.description}
-        </h3>
+        <Link to={`product/${product.id}`}>
+            <h1>
+                Tittle : {product.title}
+            </h1>
+            <h2>
+                Prce : {product.price}
+            </h2>
+            <h3>
+                Description : {product.description}
+            </h3>
+        </Link>
     </div>
   )
 }
