cff6d35be39769a01324b52cc673bf29f6169b5d
huzidev <huzaifa.iqdev@gmail.com>
2022-06-26 11:57:25 +0500

    files imported

--- mySQL/FullStack-Web/routes/reviewRouter.js
+++ mySQL/FullStack-Web/routes/reviewRouter.js
@@ -1,9 +1,9 @@
-// import reviewController from '../controllers/reviewController'
+import reviewController from '../controllers/reviewController'
 
-// const routerReview = require('express').Router()
+const routerReview = require('express').Router()
 
-// // Review's Url And Controller
-// routerReview.post('/addReview', reviewController.addReview)
-// routerReview.get('/allReviews', reviewController.getAllReviews)
+// Review's Url And Controller
+routerReview.post('/addReview', reviewController.addReview)
+routerReview.get('/allReviews', reviewController.getAllReviews)
 
-// module.exports = routerReview
+module.exports = routerReview
