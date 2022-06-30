525a67e47b8232eef0c7554e2e6c00383e4ffc2d
huzidev <huzaifa.iqdev@gmail.com>
2022-06-30 22:35:45 +0500

    props

--- express/jwt-token/client/src/App.js
+++ express/jwt-token/client/src/App.js
@@ -32,7 +32,7 @@ function App() {
           <Alert message="This is amazing React course" />
           <div className="container">
             <Routes>
-              <Route exact path="/" element={<Home />}/>
+              <Route exact path="/" element={<Home showAlert={showAlert}/>}/>
               <Route exact path="/about" element={<About  />}/>
               <Route exact path="/login" element={<Login  />}/>
               <Route exact path="/signup" element={<Signup />}/>
