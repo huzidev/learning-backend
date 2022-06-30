ba4bf665ba6b5ad84325fc04d7279ec186a0ecaf
huzidev <huzaifa.iqdev@gmail.com>
2022-06-30 18:00:03 +0500

    fix

--- express/a/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import App from "./MyComponents/App"
-import "./styles/style.css"
-
-ReactDOM.render(<App />, document.getElementById("head"))
