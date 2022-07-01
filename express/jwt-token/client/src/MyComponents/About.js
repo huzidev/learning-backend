e821545aead8eb3b85327bccd74e63bb9be99fd1
huzidev <huzaifa.iqdev@gmail.com>
2022-07-01 12:43:19 +0500

    jwt token workes

--- express/jwt-token/client/src/MyComponents/About.js
+++ express/jwt-token/client/src/MyComponents/About.js
@@ -5,31 +5,40 @@ import api from '../service/api';
 const About = () => {
     
     const Navigate = useNavigate();
-    const [data, setData] = React.useState({})
+    const [userData, setUserData] = React.useState({});
     const host = "http://localhost:5000"
 
-    React.useEffect(() => {
-
-        const getUserData = async () => {
-            const {data} = await fetch(`${host}/api/auth/getuser`, {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
+    const CallAboutPage = async () => {
+        try{
+            const res = await fetch(`${host}/api/auth/getuser`, {
+                method : "GET",
+                headers : {
+                    Accept : "application/json", // here we are not using POST we are using GEt therefore accept type is application/json for reading
+                    "Content-Type" : "application/json",
                     "auth-token": localStorage.getItem('token')
                 },
-            })    
-            const json = await data.json()
+                // credentials : "include" // so cookies could reach backend easily
+            });
+
+            const data = await res.json(); // for getting user's data
             console.log(data);
-            setData(data.name)
-            setData(data.email)
+            setUserData(data) // because data variable have all the data of user
+            
         }
-        getUserData();
-    })
+
+        catch (err) {
+            console.log(err);
+            Navigate('/login'); // so user can go login again
+        }
+    }
+
+    React.useEffect(() => {
+        CallAboutPage();
+    }, []); // we've to use empty array dependency here other wise the data will fetch continuously
     
     return (
         <div>
             This is About page
-            <form method='GET'>
                 <h3>
                     UserId :
                 </h3>
@@ -37,17 +46,16 @@ const About = () => {
                 </h5>
                 <hr />
                 <h3>
-                    Username : {data.name}
+                    Username : {userData.name}
                 </h3> 
                 <h5>
                 </h5>
                 <hr />
                 <h3>
-                    Email : {data.email}
+                    Email : {userData.email}
                 </h3>
                 <h5>
                 </h5>
-            </form>
         </div>
     )
 }
