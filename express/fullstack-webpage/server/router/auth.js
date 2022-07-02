b6644e91b29d5db53cb15bb720e538e24d0402af
huzidev <huzaifa.iqdev@gmail.com>
2022-07-02 17:57:56 +0500

    registration page

--- express/fullstack-webpage/server/router/auth.js
+++ express/fullstack-webpage/server/router/auth.js
@@ -11,14 +11,18 @@ const router = express.Router();
 router.use(express.urlencoded({extended : true}));
 
 
-// main work
+// for REGISTRATION
 router.post('/register', async (req, res) => {
+
     // getting all schema
     const { username, email, number, password, cpassword } = req.body;
+
     // if user left and tag empty
     if ( !username || !email || !number || !password || !cpassword ) {
         return res.status(422).json({ message : "You've left an tag empty" }) // json is used when message pop-up on window page as alert so ours web browser could understand it
     }
+
+    // try catch the error
     try{
         // if user is already exists
     }
