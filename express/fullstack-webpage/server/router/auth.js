1ad3d2215f9a70fb968b5dbf0ebf0f492b560080
huzidev <huzaifa.iqdev@gmail.com>
2022-07-02 17:52:04 +0500

    async functions

--- express/fullstack-webpage/server/router/auth.js
+++ express/fullstack-webpage/server/router/auth.js
@@ -1,4 +1,5 @@
 import express  from "express";
+import User from "../models/userSchema";
 
 // WE CREATE ROUTER JUST TO MAKE OURS CODE SIMPLE AND EASY TO UNDERSTAND JUST LIKE STYLE COMPONENTS OF REACT
 require('../db/connection.js');
@@ -8,6 +9,12 @@ const router = express.Router();
 
 
 router.use(express.urlencoded({extended : true}));
+
+
+// main work
+router.post('/register', async (req, res) => {
+    const {username, email, number, password, cpassword} = req.body;
+})
 
 
 router.get('/', (req, res) => {
