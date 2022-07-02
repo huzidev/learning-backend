b51d25011114805df314518fe4f867dfb41f6895
huzidev <huzaifa.iqdev@gmail.com>
2022-07-02 18:49:41 +0500

    react router

---
+++ express/fullstack-webpage/client/src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+} from "react-router-dom";
+
+export default function App() {
+    return (
+        <div>
+
+        </div>
+    )
+}
