31d3f96745b4ab09f5cd5437e0ef6979e94992c4
huzidev <huzaifa.iqdev@gmail.com>
2022-07-02 22:51:41 +0500

    verifying tokens

--- express/fullstack-webpage/server/middleware/Verification.js
+++ express/fullstack-webpage/server/middleware/Verification.js
@@ -3,10 +3,19 @@ import jwt from 'jsonwebtoken';
 
 const Verification = async (req, res, next) => {
     try{
-        // getting stored token from cookies
+        // getting stored token from cookies therefore we've used req
         const token = req.cookies.jwtoken;
 
         // verifying user
+        const verifyUser = jwt.verify(token, process.env.SECRET_KEY);
+        let verifyToken = verifyUser._id;
+
+        // finding user data through token
+        const userInfo = await User.findOne({ 
+            _id : verifyToken, 
+            // comparing token from cookies with the token stored in the backend
+            "tokens.token" : token 
+        })
     }
     catch (err) {
         console.log(err);
