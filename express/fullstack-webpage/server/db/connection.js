c417a0c7338d8b3f8a439410171e75ebb5b224de
huzidev <huzaifa.iqdev@gmail.com>
2022-07-02 17:16:31 +0500

    linking DB

---
+++ express/fullstack-webpage/server/db/connection.js
@@ -0,0 +1,12 @@
+import mongoose, { modelNames } from "mongoose";
+
+const DB = process.env.DATA
+
+mongoose.connect(DB, {
+    useNewUrlParser : true,
+    useUnifiedTopology : true,
+}).then(() => {
+    console.log("Connection Successful!");
+}).catch((err) => {
+    console.log("Failed To Connect", err);
+})
