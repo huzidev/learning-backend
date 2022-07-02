bf33a8b2f5bd85972e1af6fc227830d7f61812b5
huzidev <huzaifa.iqdev@gmail.com>
2022-07-02 22:48:24 +0500

    req cookies

--- express/fullstack-webpage/server/middleware/Verification.js
+++ express/fullstack-webpage/server/middleware/Verification.js
@@ -1,2 +1,14 @@
 import User from '../models/userSchema';
 import jwt from 'jsonwebtoken';
+
+const Verification = async (req, res, next) => {
+    try{
+        // getting stored token from cookies
+        const token = req.cookies.jwtoken;
+
+        // verifying user
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
