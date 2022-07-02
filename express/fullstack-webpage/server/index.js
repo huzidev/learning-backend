b84375ba3464dcd0c84fa03da6dd253ca84d7057
huzidev <huzaifa.iqdev@gmail.com>
2022-07-02 17:30:27 +0500

    database linked

--- express/fullstack-webpage/server/index.js
+++ express/fullstack-webpage/server/index.js
@@ -1,15 +1,22 @@
 import express from 'express';
+import dotenv from 'dotenv';
 
+
+dotenv.config({ path : './config.env' })
+require('./db/connection.js');
 const server = express();
 const port = 8000;
 
+
 server.use(express.json());
 server.use(express.urlencoded({extended : true}));
+
 
 // main work
 server.get('/', (req, res) => {
     res.json({ message : 'hello' });
 });
+
 
 // running on port
 server.listen(port, (err) => {
