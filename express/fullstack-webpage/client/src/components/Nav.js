061759477958df4b4a4cc109516608a445187f40
huzidev <huzaifa.iqdev@gmail.com>
2022-07-02 18:54:40 +0500

    nav-bar added

--- express/fullstack-webpage/client/src/components/Nav.js
+++ express/fullstack-webpage/client/src/components/Nav.js
@@ -1,10 +1,11 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Nav() {
     return (
         <div>
             <Link to='/'>Home</Link>
+            <Link to='/register'>Register</Link>
         </div>
     )
 }
