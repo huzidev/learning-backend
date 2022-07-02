e5034e3a25f6d7b3b109e0d6cae859f3f77490c5
huzidev <huzaifa.iqdev@gmail.com>
2022-07-02 12:19:29 +0500

    logout button

--- express/webpage/client/src/MyComponents/Nav.js
+++ express/webpage/client/src/MyComponents/Nav.js
@@ -9,6 +9,7 @@ export default function Nav() {
             <Link to="/contact">Contact Us</Link>
             <Link to="/login">Login</Link>
             <Link to="/register">Register</Link>
+            <Link to="/logout">Logout</Link>
         </div>
     )
 }
