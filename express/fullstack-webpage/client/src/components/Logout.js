782df9eaed112f63ad1f0f2ed4748cb978238c94
huzidev <huzaifa.iqdev@gmail.com>
2022-07-03 19:57:47 +0500

    logot functionality

--- express/fullstack-webpage/client/src/components/Logout.js
+++ express/fullstack-webpage/client/src/components/Logout.js
@@ -1,6 +1,21 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function Logout() {
+
+    const Navigate = useNavigate();
+
+    React.useEffect(() => {
+        fetch('/logout', {
+            method : 'GET',
+            headers : {
+                Accept : "application/json",
+                "Content-Type" : "application/json"
+            },
+            credentials : "include"
+        })
+    })
+
     return (
         <div>
             Logout Page
