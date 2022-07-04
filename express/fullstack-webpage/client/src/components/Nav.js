18422b7351078d1954567e77ecd127a8e183bc19
huzidev <huzaifa.iqdev@gmail.com>
2022-07-04 19:03:19 +0500

    adding animations

--- express/fullstack-webpage/client/src/components/Nav.js
+++ express/fullstack-webpage/client/src/components/Nav.js
@@ -10,14 +10,14 @@ export default function Nav() {
         <div className='nav'>
             <div className='nav-bar'>
                 <div className='right-side'>
-                    <Link to='/'>Home</Link>
+                    <Link to='/' className='animation'>Home</Link>
                 </div>
                 <div className='left-side'>
                     {isLoggedIn ? <Link to='/logout'>Logout</Link>
                     : (
                         <>
-                            <Link to='/login'>Login</Link>
-                            <Link to='/register'>Register</Link>    
+                            <Link to='/login' className='animation'>Login</Link>
+                            <Link to='/register' className='animation'>Register</Link>    
                         </>
                     )}
                 </div>
