ec70420ecc26e7eea595cf4c2f15da3290edd2c0
huzidev <huzaifa.iqdev@gmail.com>
2022-07-04 22:18:13 +0500

    user about page fixed

--- express/fullstack-webpage/client/src/components/About.js
+++ express/fullstack-webpage/client/src/components/About.js
@@ -1,10 +1,7 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
 
 export default function About() {
 
-    const Navigate = useNavigate();
-    
     const [useData, setUserData] = React.useState({})
 
     async function aboutPage() {
