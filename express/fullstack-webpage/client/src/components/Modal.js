c588be9c587847d1f2f234cc901e9b7b8eebd0d9
huzidev <huzaifa.iqdev@gmail.com>
2022-07-05 22:58:39 +0500

    link

--- express/fullstack-webpage/client/src/components/Modal.js
+++ express/fullstack-webpage/client/src/components/Modal.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Modal() {
     return (
         <div className='modal'>
             <h5>
-                Register as Seller
+                Register as <Link to='/register/seller'>Seller </Link>
             </h5>
             <h5>
                 Register as Buyer
