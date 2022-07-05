a263dd180b931a6242be1dba26aeb268505357fd
huzidev <huzaifa.iqdev@gmail.com>
2022-07-05 18:37:42 +0500

    default date added

--- express/fullstack-webpage/server/models/userMessage.js
+++ express/fullstack-webpage/server/models/userMessage.js
@@ -1,1 +1,23 @@
 import mongoose from 'mongoose';
+
+const userMessage = new mongoose.Schema({
+    username : {
+        type : String,
+        required : true
+    },
+    email : {
+        type : String,
+        required : true
+    },
+    number : {
+        type : Number,
+    },
+    message : {
+        type : String,
+        required : true,
+    },
+    date : {
+        type : Date,
+        default : Date.now // no need for using () for date.now()
+    }
+})
