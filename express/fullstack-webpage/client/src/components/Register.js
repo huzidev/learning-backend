eb4fa87475c5b1fa11196afa3a881653b684bee2
huzidev <huzaifa.iqdev@gmail.com>
2022-07-06 12:44:17 +0500

    useSelector hook for state

--- express/fullstack-webpage/client/src/components/Register.js
+++ express/fullstack-webpage/client/src/components/Register.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Modal from './Modal';
 
 export default function Register() {
+
+    const dispatch = useDispatch();
+
+    const registerBuyer = useSelector((state) => state.register.registerBuyer);
+    const registerSeller = useSelector((state) => state.register.registerSeller);
+
     return (
         <div>
             <Modal />      
