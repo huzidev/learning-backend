8f5a1a6893a8e29c141be5992c01852314e4c39c
huzidev <huzaifa.iqdev@gmail.com>
2022-07-06 12:42:35 +0500

    main redux store

--- express/fullstack-webpage/client/src/store/index.js
+++ express/fullstack-webpage/client/src/store/index.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import logInStore from "./Login-Store";
+import registerStore from "./Register-Store";
 
 const store = configureStore({
     reducer : {
-        login : logInStore.reducer
+        login : logInStore.reducer,
+        register : registerStore.reducer
     }
 });
 
