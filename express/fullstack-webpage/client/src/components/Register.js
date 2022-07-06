1eb5207a4477287a43ed48d7bb6719ed46622860
huzidev <huzaifa.iqdev@gmail.com>
2022-07-06 12:50:00 +0500

    function for navigate

--- express/fullstack-webpage/client/src/components/Register.js
+++ express/fullstack-webpage/client/src/components/Register.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import registerStore from '../store/Register-Store';
 import Modal from './Modal';
 
 export default function Register() {
@@ -9,9 +11,29 @@ export default function Register() {
     const registerBuyer = useSelector((state) => state.register.registerBuyer);
     const registerSeller = useSelector((state) => state.register.registerSeller);
 
+    function buyer() {
+        dispatch(registerStore.regBuyer())
+    }
+    function seller() {
+        dispatch(registerStore.regSeller())
+    }
     return (
         <div>
-            <Modal />      
+            <Modal />
+            { 
+                registerBuyer 
+                    ? (
+                        <Link to='/register/buyer' onClick={}>Buyer</Link>
+                    )
+                    : ''
+            }
+            { 
+                registerSeller
+                    ? (
+                        <Link to='/register/seller'>Seller</Link>
+                    )
+                    : ''
+            }
         </div>
     )
 }
