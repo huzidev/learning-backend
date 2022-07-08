d03de1c03064dbbf05d5cb022c46a950d88ba855
huzidev <huzaifa.iqdev@gmail.com>
2022-07-08 17:27:13 +0500

    info for adding products

--- express/fullstack-webpage/server/controllers/productController.js
+++ express/fullstack-webpage/server/controllers/productController.js
@@ -1,1 +1,14 @@
 import db from '../models/index.js';
+
+const Product = db.products;
+const Review = db.reviews;
+
+// ADD PRODUCT
+const addProduct = async (req, res) => {
+    let info = {
+        image : req.file.path,
+        title : req.body.title,
+        price : req.body.price,
+        description : req.body.description
+    }
+}
