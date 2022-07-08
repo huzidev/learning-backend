68ea0c972ad659093910f75927fae6f5761fe533
huzidev <huzaifa.iqdev@gmail.com>
2022-07-08 20:03:21 +0500

    get all review

---
+++ express/fullstack-webpage/server/router/reviewRouter.js
@@ -0,0 +1,8 @@
+import reviewController from '../controllers/reviewController';
+
+const routerReview  = require('express').Router()
+
+// for adding review
+routerReview.post('/addReview', reviewController.addReview)
+
+// for getting all reviews respective to the product
