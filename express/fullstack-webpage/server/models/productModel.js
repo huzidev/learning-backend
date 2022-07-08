6008b62a05a832e522005161a1b28df50520d8fe
huzidev <huzaifa.iqdev@gmail.com>
2022-07-08 15:59:31 +0500

    image type string

--- express/fullstack-webpage/server/models/productModel.js
+++ express/fullstack-webpage/server/models/productModel.js
@@ -2,7 +2,13 @@
 module.exports = { sequelize ,DataTypes } => { // we are not importing sequelize therefore we've used sequelize with small s
     const Product = sequelize.define("product", {
         pid : {
-            type : this.DataTypes.INTEGER
+            type : DataTypes.INTEGER,
+            allowNull : false,
+            autoIncrement : true,
+            primaryKey : true
+        },
+        image : {
+            type : DataTypes.STRING
         }
     }) 
 }
