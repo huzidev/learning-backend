5c77fe6f0a3154c741edcb9b2e67d31c35d5e8e8
huzidev <huzaifa.iqdev@gmail.com>
2022-07-08 17:35:00 +0500

    update product

--- express/fullstack-webpage/server/controllers/productController.js
+++ express/fullstack-webpage/server/controllers/productController.js
@@ -11,4 +11,35 @@ const addProduct = async (req, res) => {
         price : req.body.price,
         description : req.body.description
     }
+    
+    const product = await Product.create(info)
+    res.status(200).send(product)
+}
+
+// GET ALL PRODUCTS
+const getAllProducts = async (req, res) => {
+    let products = await Product.findAll({}) // since we will GET all products therefore we've used ({}) object
+    res.status(200).send(products)
+}
+
+
+// GET SINGLE PRODUCT
+
+const getOneProduct = async (req, res) => {
+    // id for specific product
+    let id = req.params.id
+    let product = await Product.findOne({
+        where : {
+            pid : id // pid is for product id
+        }
+    })
+    res.status(200).send(product)
+}
+
+// UPDATE PRODUCT
+const updateProduct = async (req, res) => {
+    let id = req.params.id
+    const product = await Product.update(req.body, { // req body for getting data about respective product
+
+    })
 }
