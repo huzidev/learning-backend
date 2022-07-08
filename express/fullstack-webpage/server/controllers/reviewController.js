a9fb442531d623a7842edea12d08ba438e1aa1de
huzidev <huzaifa.iqdev@gmail.com>
2022-07-08 18:28:44 +0500

    description and infp

--- express/fullstack-webpage/server/controllers/reviewController.js
+++ express/fullstack-webpage/server/controllers/reviewController.js
@@ -1,2 +1,14 @@
 // FOR MYSQL
-import db from '../models/index.js'
+import db from '../models/index.js';
+
+const Review = db.reviews
+
+// ADD REVIEW
+const addReview = async (req, res) => {
+    const id = req.body.pid // means inside body of that specific product's id in which we wanted to add review
+    let data = {
+        pid : id,
+        rating : req.body.rating,
+        description : req.body.description
+    }
+}
