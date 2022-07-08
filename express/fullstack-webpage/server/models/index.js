6a592e45d59f471c76539dc97b5639219f2fc379
huzidev <huzaifa.iqdev@gmail.com>
2022-07-08 16:08:42 +0500

    force type for resync

--- express/fullstack-webpage/server/models/index.js
+++ express/fullstack-webpage/server/models/index.js
@@ -34,3 +34,10 @@ db.sequelize = sequelize
 
 db.products = require('./productModel.js')(sequelize, DataTypes) // inside bracket we've same parameters as we used in product and reviews schema or models
 db.reviews = require('./reviewModel.js')(sequelize, DataTypes)
+
+db.sequelize.sync({ force : false })
+.then(() => {
+    console.log("Re-Sync done");
+})
+// if we didn't do this than every time we run the server the force will overwrite all the 
+// data present in the table with new data and we don't want that until we really wanted it therefore it is necessary to use force : false
