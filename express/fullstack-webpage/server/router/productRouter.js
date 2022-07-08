3829111f0b1a64957d7e26526cf580f19e3952ad
huzidev <huzaifa.iqdev@gmail.com>
2022-07-08 18:48:23 +0500

    product controller fix

--- express/fullstack-webpage/server/router/productRouter.js
+++ express/fullstack-webpage/server/router/productRouter.js
@@ -2,14 +2,24 @@ import productController from '../controllers/productController';
 
 const routerProduct = require('express').Router()
 
+// for adding product
 routerProduct.post('/addProduct', productController.upload, productController.addProduct) // productController.upload for uploading images
 
+// for getting all products
 routerProduct.get('/allProducts', productController.getAllProducts)
 
+// for getting single product with specific reviews about that
 routerProduct.get('/:id', productController.getOneProduct)
 
+// for getting reviews about specific product
+routerProduct.get('/productReviews/:id', productController.getProductReviews)
+
+// for updating product
 routerProduct.put('/:id', productController.updateProduct)
 
+// for deleting product
 routerProduct.delete('/:id', productController.deleteProduct)
+
+
 
 module.exports = routerProduct
