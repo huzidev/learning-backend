f5b839a44004f781ac367a7282ef7e671d3f0c30
huzidev <huzaifa.iqdev@gmail.com>
2022-07-08 15:58:05 +0500

    integer data type

--- express/fullstack-webpage/server/models/productModel.js
+++ express/fullstack-webpage/server/models/productModel.js
@@ -1,4 +1,8 @@
 // FOR MYSQL
 module.exports = { sequelize ,DataTypes } => { // we are not importing sequelize therefore we've used sequelize with small s
-
+    const Product = sequelize.define("product", {
+        pid : {
+            type : this.DataTypes.INTEGER
+        }
+    }) 
 }
