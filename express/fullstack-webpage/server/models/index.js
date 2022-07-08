0a790c9f9cdff820166dacc6f96445d9d00f19c0
huzidev <huzaifa.iqdev@gmail.com>
2022-07-08 15:46:58 +0500

    configurations linked

--- express/fullstack-webpage/server/models/index.js
+++ express/fullstack-webpage/server/models/index.js
@@ -1,3 +1,20 @@
 // FOR MYSQL
 import { Sequelize, DataTypes } from 'sequelize';
 import dbConfig from '../config/dbConfig';
+
+const sequelize = new Sequelize(
+    dbConfig.DB,
+    dbConfig.USER,
+    dbConfig.PASSWORD, {
+        host : dbConfig.HOST,
+        dialect : dbConfig.dialect,
+        operatorsAliases : false,
+
+        pool : {
+            max : dbConfig.pool.max,
+            min : dbConfig.pool.min,
+            acquire : dbConfig.pool.acquire,
+            idle : dbConfig.pool.idle
+        }
+    }
+)
