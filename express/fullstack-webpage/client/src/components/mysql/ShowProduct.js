c9ead896c4f7273360031506df56fb3a5d75e2d5
huzidev <huzaifa.iqdev@gmail.com>
2022-07-14 17:57:02 +0500

    sass to css completed

--- express/fullstack-webpage/client/src/components/mysql/ShowProduct.js
+++ express/fullstack-webpage/client/src/components/mysql/ShowProduct.js
@@ -11,6 +11,9 @@ export default function ShowProduct() {
                 <h1>
                     All Products
                 </h1>
+                {/* we've created the separate components for showProducts with (s) and showProduct without (s)
+                is because we don't wanted to show the heading and header component when user is at home page
+                 */}
                 <ShowProducts />
             </div>
         </>
