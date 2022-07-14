0e61f6030430b279dea46126e7bf376129cc8f03
huzidev <huzaifa.iqdev@gmail.com>
2022-07-14 13:36:10 +0500

    css error fix

--- express/fullstack-webpage/client/src/index.js
+++ express/fullstack-webpage/client/src/index.js
@@ -2,6 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import { Provider } from 'react-redux';
+import "./styles/style.css"
+import "./styles/Home.css"
+import "./styles/AddProduct.css"
+import "./styles/Info.css"
+import "./styles/Login.css"
+import "./styles/Modal.css"
+import "./styles/Nav.css"
+import "./styles/Register.css"
+import "./styles/SellerData.css"
+import "./styles/ShowProducts.css"
+import "./styles/Footer.css"
 import store  from './store';
 
 ReactDOM.render(
