fadc1cb19b6a959e40fedea38c4d7e649fdb59be
huzidev <huzaifa.iqdev@gmail.com>
2022-07-14 22:46:38 +0500

    sellers name added

--- express/fullstack-webpage/client/src/components/mongodb/SellerData.js
+++ express/fullstack-webpage/client/src/components/mongodb/SellerData.js
@@ -7,16 +7,16 @@ export default function SellerData() {
 
     const { id } = useParams()
 
-    const [sellersData, setSellersData] = React.useState({
-        // _id : '',
-        // username : '',
-        // email : '',
-        // number : '',
-        // sellerinfo : []
-    })
+    const [sellersData, setSellersData] = React.useState({})
 
     async function getData() {
         try{
+            console.log("id");
+            console.log("id");
+            console.log("id");
+            console.log("id");
+            console.log("id");
+            console.log(id);
             const res = await fetch(`/allSellers/${id}`, {
                 method : "GET",
                 headers : {
@@ -36,63 +36,19 @@ export default function SellerData() {
     React.useEffect(() => {
             getData()
     }, [])
-    
-
-    // const displayData = (data) => {
-    //     if (!data) {
-    //         return
-    //     }
-
-    //     return data.map((info, index) => (
-    //         <div key={index} className="user-info-container">
-    //             <div className="user-info">
-    //                 <h5>
-    //                     Seller's ID :&nbsp;
-    //                 </h5>
-    //                 <p>
-    //                     {info._id}
-    //                 </p>
-    //             </div>
-    //             <div className="user-info">
-    //                 <h5>
-    //                     Seller's Name :&nbsp;
-    //                 </h5>
-    //                 <p>
-    //                     {info.username}
-    //                 </p>
-    //             </div>
-    //             <div className="user-info">
-    //                 <h5>
-    //                     Seller's Email :&nbsp;
-    //                 </h5>
-    //                 <p>
-    //                     {info.email}
-    //                 </p>
-    //             </div>
-    //             <div className="user-info">
-    //                 <h5>
-    //                     Seller's Number :&nbsp;
-    //                 </h5>
-    //                 <p>
-    //                     {info.number}
-    //                 </p>
-    //             </div>
-    //         </div>
-    //     ))
-    // }
 
     return (
         <>
             <Header />
             <div className="seller-info">
                 <h3 className='heading'>
-                    List of all sellers
+                    About {sellersData.username}
                 </h3>
                 <div className='seller-data'>
-                    {/* {
-                        displayData(sellersData.sellerinfo)
-                    } */}
+                    ID : {sellersData._id}
                     name : {sellersData.username}
+                    email : {sellersData.email}
+                    number : {sellersData.number}
                 </div>
             </div>
         </>
