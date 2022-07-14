1ae1567a41cdbc851fb9ba6be478e57b86f9362d
huzidev <huzaifa.iqdev@gmail.com>
2022-07-14 23:07:31 +0500

    navigate fix

--- express/fullstack-webpage/client/src/components/Header.js
+++ express/fullstack-webpage/client/src/components/Header.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function Header() {
+
+    const Navigate = useNavigate()
+
+    function backButton() {
+        Navigate(-1);
+    }
+
     return (
         <div className='nav'>
             <div  className='nav-bar'>
                 <Link to='/' className='animation'>Home</Link>
+                <button onClick={backButton}>
+                    back
+                </button>
             </div>
         </div>
     )
