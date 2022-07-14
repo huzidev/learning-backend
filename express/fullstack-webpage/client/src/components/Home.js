3f70c8eb6e19e3d86f58034fb3a8c9332979ba9c
huzidev <huzaifa.iqdev@gmail.com>
2022-07-14 18:02:37 +0500

    footer fix

--- express/fullstack-webpage/client/src/components/Home.js
+++ express/fullstack-webpage/client/src/components/Home.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import Nav from './Nav'
 import ShowProducts from './mysql/ShowProducts'
-// import Footer from './Footer';
+import Footer from './Footer';
 
 export default function Home() {
     return (
         <>
             <Nav />
             <div className='home-page'>
-                <h1>
+                <h1 className="home-page-heading">
                     Items For Sale
                 </h1>
                 <ShowProducts />
             </div>
-            {/* <Footer /> */}
+            <Footer />
         </>
     )
 }
