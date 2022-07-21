abf747b05cf1fdc34d8044c17b3a98bae2d4d683
huzidev <huzaifa.iqdev@gmail.com>
2022-07-21 14:09:14 +0500

    tooltips added

--- express/fullstack-webpage/client/src/components/Footer.js
+++ express/fullstack-webpage/client/src/components/Footer.js
@@ -66,63 +66,73 @@ export default function Footer() {
                     </h2>
                     <div className='footer-items'>
                         <div className="parts">
-                            <div className="logo">
-                                <i className="fab fa-github">
-                                </i>
-                                <a 
-                                    href="https://github.com/huzidev" 
-                                    target="_blank" 
-                                    rel="noopener noreferrer" 
-                                    // it is necessary to use rel property
-                                >
-                                    Huzi-Dev
-                                </a>
-                            </div>
-                            <div className="logo">
-                                <i className="fa fa-envelope">
-                                </i>
-                                <a 
-                                    href="https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=huzi.iqdev@gmail.com" 
-                                    target="_blank" 
-                                    rel="noopener noreferrer"
-                                >
-                                    Huzaifa Iqbal
-                                </a>
-                            </div>
+                            <Tooltip title="Github">
+                                <div className="logo">
+                                        <i className="fab fa-github">
+                                        </i>
+                                    <a 
+                                        href="https://github.com/huzidev" 
+                                        target="_blank" 
+                                        rel="noopener noreferrer" 
+                                        // it is necessary to use rel property
+                                        >
+                                        Huzi-Dev
+                                    </a>
+                                </div>
+                            </Tooltip>
+                            <Tooltip title="Gmail">
+                                <div className="logo">
+                                        <i className="fa fa-envelope">
+                                        </i>
+                                    <a 
+                                        href="https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=huzi.iqdev@gmail.com" 
+                                        target="_blank" 
+                                        rel="noopener noreferrer"
+                                        >
+                                        Huzaifa Iqbal
+                                    </a>
+                                </div>
+                            </Tooltip>
                         </div>
                         <div className="parts">
-                            <div className="logo">
-                                <i className="fa fa-facebook">
-                                </i>
-                                <a 
-                                    href="https://facebook.com" 
-                                    target="_blank" 
-                                    rel="noopener noreferrer"
-                                >
-                                    Huzaifa Iqbal
-                                </a>
-                            </div>
-                            <div className="logo">
-                                <i className="fa fa-instagram">
-                                </i>
-                                <a 
-                                    href="https://instagram.com" 
-                                    target="_blank" 
-                                    rel="noopener noreferrer"
-                                >
-                                    huzaifa_iqbal_
-                                </a>
-                            </div>
+                            <Tooltip title="Facebook">
+                                <div className="logo">
+                                        <i className="fa fa-facebook">
+                                        </i>
+                                    <a 
+                                        href="https://facebook.com" 
+                                        target="_blank" 
+                                        rel="noopener noreferrer"
+                                    >
+                                        Huzaifa Iqbal
+                                    </a>
+                                </div>
+                            </Tooltip>
+                            <Tooltip title="Instagram">
+                                <div className="logo">
+                                        <i className="fa fa-instagram">
+                                        </i>
+                                    <a 
+                                        href="https://instagram.com" 
+                                        target="_blank" 
+                                        rel="noopener noreferrer"
+                                        >
+                                        huzaifa_iqbal_
+                                    </a>
+                                </div>
+                            </Tooltip>
                         </div>
                         <div className="bottom">
-                            <div className="logo">
-                                <i className="fa fa-phone">
-                                </i>
-                                <a href="tel:0303030303"> 
-                                {/* if we uses (tel) then it'll directly opens phone app for you */}
-                                    123-456-789
-                                </a>
-                            </div>
+                            <Tooltip title="Phone">
+                                <div className="logo">
+                                        <i className="fa fa-phone">
+                                        </i>
+                                    <a href="tel:0303030303"> 
+                                    {/* if we uses (tel) then it'll directly opens phone app for you */}
+                                        123-456-789
+                                    </a>
+                                </div>
+                            </Tooltip>
                         </div>
                     </div>
                     <form action="POST">
