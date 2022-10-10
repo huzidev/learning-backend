f4510b3912e537c45ad78fe5606dd5a95085247c
huzidev <huzaifa.iqdev@gmail.com>
2022-10-10 20:19:26 +0500

    process env

--- express/todo-webpage/server/middleware/Verification.js
+++ express/todo-webpage/server/middleware/Verification.js
@@ -4,6 +4,7 @@ import User from '../models/userSchema';
 const Verification = async (req, res, next) => {
     try {
         const token = req.cookies.jwtoken;
+        const verifyUser = jwt.verify(token, process.env.SECRET_KEY);
     } catch (err) {
         res.status(401).send("Unauthorized : No token provided");
         console.log(err);
