258f22f9a21e0c129f298709087db35f9ea9c3c0
huzidev <huzaifa.iqdev@gmail.com>
2022-10-10 20:18:44 +0500

    cookies

--- express/todo-webpage/server/middleware/Verification.js
+++ express/todo-webpage/server/middleware/Verification.js
@@ -3,7 +3,7 @@ import User from '../models/userSchema';
 
 const Verification = async (req, res, next) => {
     try {
-
+        const token = req.cookies.jwtoken;
     } catch (err) {
         res.status(401).send("Unauthorized : No token provided");
         console.log(err);
