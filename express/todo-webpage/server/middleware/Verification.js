6780da5287a2682b80e1d148e939d2c651b237ae
huzidev <huzaifa.iqdev@gmail.com>
2022-10-10 20:23:33 +0500

    error 404

--- express/todo-webpage/server/middleware/Verification.js
+++ express/todo-webpage/server/middleware/Verification.js
@@ -5,8 +5,17 @@ const Verification = async (req, res, next) => {
     try {
         const token = req.cookies.jwtoken;
         const verifyUser = jwt.verify(token, process.env.SECRET_KEY);
+        let tok = verifyUser._id;
+        const userInfo = await User.findOne({
+            _id: tok,
+            "tokens.token": token // in mongoDB we've tokens in which token and :token is defined here
+        })
+
+        if (!userInfo) {
+            res.status(404).send("User Not Found");
+        }
     } catch (err) {
-        res.status(401).send("Unauthorized : No token provided");
+        res.status(401).send("No token provided");
         console.log(err);
     }
 }
