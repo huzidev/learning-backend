d8b292ca73140f1659aad22d72cfa49e914dc846
huzidev <huzaifa.iqdev@gmail.com>
2022-10-10 18:38:58 +0500

    mongoose connection added

--- express/todo-webpage/server/db/connection.js
+++ express/todo-webpage/server/db/connection.js
@@ -1,1 +1,12 @@
 import mongoose from "mongoose";
+
+const DB = process.env.DATA;
+
+mongoose.connect(DB, {
+    useuseNewUrlParser : true,
+    useUnifiedTopology : true
+}).then(() => {
+    console.log("Connection Successfull");
+}).catch((e) => {
+    console.log("Error" + e);
+})
