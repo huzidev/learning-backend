be29633ae3d010e84a0da2b154a211dd7d001b95
huzidev <huzaifa.iqdev@gmail.com>
2022-10-10 17:06:13 +0500

    basic node js server

--- express/todo-webpage/server/index.js
+++ express/todo-webpage/server/index.js
@@ -2,3 +2,16 @@ import express from "express";
 
 const server = express();
 const port = 8000;
+
+server.post('/', (req, res) => {
+    res.send("Hello, World");
+})
+
+server.listen(port, (err) => {
+    if (!err) {
+        console.log(`Server is running on port ${port}`);
+    }
+    else {
+        console.log(err);
+    }
+})
