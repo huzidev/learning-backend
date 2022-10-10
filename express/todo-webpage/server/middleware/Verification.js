a7f256858ad59bfa2acabf9c442de46032c8b962
huzidev <huzaifa.iqdev@gmail.com>
2022-10-10 20:18:16 +0500

    try catch

--- express/todo-webpage/server/middleware/Verification.js
+++ express/todo-webpage/server/middleware/Verification.js
@@ -1,2 +1,11 @@
 import jwt from 'jsonwebtoken';
 import User from '../models/userSchema';
+
+const Verification = async (req, res, next) => {
+    try {
+
+    } catch (err) {
+        res.status(401).send("Unauthorized : No token provided");
+        console.log(err);
+    }
+}
