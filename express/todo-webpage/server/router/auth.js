73ccb0752e359610d7ee373395bd74fe603464d8
huzidev <huzaifa.iqdev@gmail.com>
2022-10-10 18:42:30 +0500

    try catch error

--- express/todo-webpage/server/router/auth.js
+++ express/todo-webpage/server/router/auth.js
@@ -1,4 +1,15 @@
 import express from "express";
 
+
 const router = express.router();
 require('../db/connection');
+
+router.use(express.urlencoded({ extended : false }));
+
+router.post('/register', async (req, res) => {
+    try {
+
+    } catch (err) {
+        console.log(err);
+    }
+})
