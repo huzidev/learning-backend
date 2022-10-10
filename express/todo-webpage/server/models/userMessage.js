12c643d775980a9200017827f517f8813ab02610
huzidev <huzaifa.iqdev@gmail.com>
2022-10-10 20:12:48 +0500

    contact us page

---
+++ express/todo-webpage/server/models/userMessage.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const userMessage = new mongoose.Schema({
+
+    username : {
+        type : String,
+        required : true // means compulsory
+    },
+    email : {
+        type : String,
+        required : true
+    },
+    number : {
+        type : Number,
+        required : true
+    },
+    message : {
+        type : String,
+        required : true,
+    },
+    date : {
+        type : Date,
+        default : Date.now // no need for using () for date.now()
+    }
+   
+})
+
+const Contact = mongoose.model('USERS_MESSAGES', userMessage); 
+
+module.exports = Contact;
