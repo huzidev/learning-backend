0ec07fb1714e8bd8f4c20e72b7edf58e8301d61f
huzidev <huzaifa.iqdev@gmail.com>
2022-10-11 15:26:35 +0500

    React use state

--- express/todo-webpage/client/src/components/Form/Signup.js
+++ express/todo-webpage/client/src/components/Form/Signup.js
@@ -1,6 +1,15 @@
 import React from 'react';
 
 export default function Signup() {
+
+  const [user, setUser] = React.useState({
+    username : "",
+        email : "",
+        number : "",
+        password : "",
+        cpassword : ""
+  })
+
   return (
     <div>
 
