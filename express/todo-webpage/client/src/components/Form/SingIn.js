88d310d12f62e7cfb550727725462950e12e5780
huzidev <huzaifa.iqdev@gmail.com>
2022-10-11 21:18:58 +0500

    response status

--- express/todo-webpage/client/src/components/Form/SingIn.js
+++ express/todo-webpage/client/src/components/Form/SingIn.js
@@ -7,7 +7,9 @@ export default function SingIn() {
   const host = "http://localhost:8000";
 
   const [user, setUser] = React.useState({
-    username : "",
+    username: "",
+    number : "",
+    email: "",
     password : ""
   });
 
@@ -22,33 +24,60 @@ export default function SingIn() {
     });
   };
 
+  let hold;
+  let type;
+  let namea;
+  let state;
   async function signIn(e) {
     e.preventDefault();
 
     const { email, username, number, password } = user;
     
     const res = await fetch(`/signin`, {
-        method : "POST",
-        headers : {
-            "Content-Type" : "application/json"
-        },
-        body : JSON.stringify({
-            username,
-            password
-        })
+      method : "POST",
+      headers : {
+        "Content-Type" : "application/json"
+      },
+      body : JSON.stringify({
+        username,
+        email,
+        number,
+        password
+      })
     });
+    
+    if (res.status === 455) {
+      hold = email;
+      type = "email"
+      namea = "email"
+      state = user.email
+    } else if (res.status === 456) {
+      hold = username;
+      type = "text"
+      namea = "username"
+      state = user.username
+    } else if (res.status === 457) {
+      hold = number;
+      type = "number"
+      namea = "number"
+      state = user.number
+    }
 
     const data = await res.json();
     if (!data) {
       window.alert("Invalid Value!")
-    } 
-    else if (res.status === 500) {
+    } else if (res.status === 400) {
+      window.alert("Email or Password is incorrect")
+    } else if (res.status === 401) {
+      window.alert("Username or Password is incorrect")
+    } else if (res.status === 402) {
+      window.alert("Number or Password is incorrect")
+    } else if (res.status === 500) {
       window.alert("Internal Server Error : Failed to registered!")
     }
     else {
       window.alert("LoggedIn Successfully!")
       localStorage.setItem('jwtoken', data.token)
-      Navigate("/");
     }
   }
 
@@ -58,12 +87,19 @@ export default function SingIn() {
           SingIn Page
         </h1>
         <form>
+            {/* <input 
+              type={type}
+              name={namea}
+              value={state}
+              onChange={inputHandler}
+              required
+            /> */}
             <input 
               type="text"
               name="username"
               value={user.username}
               onChange={inputHandler}
-              placeholder="Enter Yours Name"
+              placeholder="Enter Yours username"
               required
             />
             <input 
