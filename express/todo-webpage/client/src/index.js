a9d732d494b97e43ba75acabd5b6ad65275a5156
huzidev <huzaifa.iqdev@gmail.com>
2022-10-11 15:21:24 +0500

    adding front end

--- express/todo-webpage/client/src/index.js
+++ express/todo-webpage/client/src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
+    <App />,
     document.getElementById("head")
 )
