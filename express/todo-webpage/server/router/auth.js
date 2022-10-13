e7245d262e34002befecaf2e447f81e5a7685db6
huzidev <huzaifa.iqdev@gmail.com>
2022-10-13 10:21:56 +0500

    logout button

--- express/todo-webpage/server/router/auth.js
+++ express/todo-webpage/server/router/auth.js
@@ -151,4 +151,9 @@ router.post('/contact', async (req, res) => {
     }
 })
 
+router.get('/logout', (req, res) => {
+    res.clearCookie('jwtoken', { path : '/' })
+    res.status(200).send("User loggedOut Successfully!")
+})
+
 module.exports = router;
