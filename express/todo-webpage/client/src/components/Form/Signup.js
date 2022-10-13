9227aeab067f0df74fe9f33b3bbf018a0d1637c9
huzidev <huzaifa.iqdev@gmail.com>
2022-10-13 23:10:25 +0500

    type password added

--- express/todo-webpage/client/src/components/Form/Signup.js
+++ express/todo-webpage/client/src/components/Form/Signup.js
@@ -163,6 +163,7 @@ export default function SignUp() {
                 rules={[
                 {
                     required: true,
+                    type: 'password',
                     min: 6
                 },
                 ]}
