5295275581a31745879a5cb35e303c2c10f3829f
huzidev <huzaifa.iqdev@gmail.com>
2022-10-13 09:36:53 +0500

    running antd

--- express/todo-webpage/client/src/components/Home.js
+++ express/todo-webpage/client/src/components/Home.js
@@ -1,9 +1,83 @@
-import React from 'react';
-
-export default function Home() {
-  return (
-    <div>
-        Home Page
-    </div>
-  )
+import {
+  DesktopOutlined,
+  FileOutlined,
+  TeamOutlined,
+  UserOutlined,
+  HomeOutlined
+} from '@ant-design/icons';
+import { Breadcrumb, Layout, Menu } from 'antd';
+import React, { useState } from 'react';
+const { Header, Content, Footer, Sider } = Layout;
+function getItem(label, key, icon, children) {
+  return {
+    key,
+    icon,
+    children,
+    label,
+  };
 }
+const items = [
+  getItem('User', '1', <UserOutlined />),
+  getItem(' 2', '2', <DesktopOutlined />),
+  getItem('Home', 'sub1', <HomeOutlined />, [
+    getItem('Grocery', '3'),
+    getItem('Bills', '4'),
+    getItem('Rent', '5'),
+  ]),
+  getItem('Office', 'sub2', <TeamOutlined />, [getItem('Project A', '6'), getItem('Project B', '8')]),
+  getItem('Completed Tasks', '9', <FileOutlined />),
+];
+const App = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  return (
+    <Layout
+      style={{
+        minHeight: '100vh',
+      }}
+    >
+      <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
+        <div className="logo" />
+        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
+      </Sider>
+      <Layout className="site-layout">
+        <Header
+          className="site-layout-background"
+          style={{
+            padding: 0,
+          }}
+        />
+        <Content
+          style={{
+            margin: '0 16px',
+          }}
+        >
+          <Breadcrumb
+            style={{
+              margin: '16px 0',
+            }}
+          >
+            <Breadcrumb.Item>User</Breadcrumb.Item>
+            <Breadcrumb.Item>Bill</Breadcrumb.Item>
+          </Breadcrumb>
+          <div
+            className="site-layout-background"
+            style={{
+              padding: 24,
+              minHeight: 360,
+            }}
+          >
+            Bill is a cat.
+          </div>
+        </Content>
+        <Footer
+          style={{
+            textAlign: 'center',
+          }}
+        >
+          Ant Design ©2018 Created by Ant UED
+        </Footer>
+      </Layout>
+    </Layout>
+  );
+};
+export default App;
