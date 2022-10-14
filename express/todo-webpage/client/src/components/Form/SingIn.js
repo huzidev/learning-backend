5ea8b7bf3e3f13e6e3a5df4b109e5f465bdedcef
huzidev <huzaifa.iqdev@gmail.com>
2022-10-14 18:02:26 +0500

    singin page

--- express/todo-webpage/client/src/components/Form/SingIn.js
+++ express/todo-webpage/client/src/components/Form/SingIn.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import { Button, Checkbox, Form, Input } from 'antd';
 
 export default function SingIn() {
 
@@ -58,31 +60,66 @@ export default function SingIn() {
     }
   }
 
+  const onFinish = (values) => {
+    console.log('Received values of form: ', values);
+  };
+
   return (
     <div>
         <h1>
           SingIn Page
         </h1>
-        <form>
-            <input 
-              type="text"
-              name="email"
-              value={user.email}
-              onChange={inputHandler}
-              required
-            />
-            <input 
+        <Form
+          name="normal_login"
+          className="login-form"
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
+        >
+          <Form.Item
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your Username!',
+              },
+            ]}
+          >
+            <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your Password!',
+              },
+            ]}
+          >
+            <Input
+              prefix={<LockOutlined className="site-form-item-icon" />}
               type="password"
-              name="password"
-              value={user.password}
-              onChange={inputHandler}
-              placeholder="Enter Yours Password"
-              required
+              placeholder="Password"
             />
-            <button onClick={signIn}>
-              SignIn
-            </button>
-        </form>
+          </Form.Item>
+          <Form.Item>
+            <Form.Item name="remember" valuePropName="checked" noStyle>
+              <Checkbox>Remember me</Checkbox>
+            </Form.Item>
+
+            <a className="login-form-forgot" href="">
+              Forgot password
+            </a>
+          </Form.Item>
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit" className="login-form-button">
+              Log in
+            </Button>
+            Or <a href="">register now!</a>
+          </Form.Item>
+        </Form>
     </div>
   )
 }
