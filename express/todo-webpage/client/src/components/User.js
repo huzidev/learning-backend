7a96db0cd6e10f9b62ea984134d28f3d9abfa3bd
huzidev <huzaifa.iqdev@gmail.com>
2022-10-15 22:31:53 +0500

    context works

--- express/todo-webpage/client/src/components/User.js
+++ express/todo-webpage/client/src/components/User.js
@@ -1,40 +1,10 @@
-// import React from 'react';
-// import TodoPage from './Todo/TodoPage';
+import React from 'react';
+import TestContext from './Context/TestContext';
 
-// export default function User() {
+export default function User() {
 
-//   const [userData, setUserData] = React.useState({})
-
-//   async function about() {
-//     try {
-//         const res = await fetch('/about', {
-//             method : 'GET',
-//                 headers : {
-//                     "Accept" : "application/json",
-//                     "Content-Type" : "application/json",
-//                 },
-//             credentials : "include"
-//         })
-//         const data = await res.json();
-//         setUserData(data);
-
-//         if (!res.status === 200) {
-//             const error = new Error(res.error)
-//             throw error;
-//         }
-//     } catch (err) {
-//         console.log(err);
-//     }
-//   }
-
-//   React.useEffect(() => {
-//     if (localStorage.getItem('jwtoken')) {
-//         about();
-//     }
-//   }, [])
-  
-//   return (
-//     <div>
-//     </div>
-//   )
-// }
+  return (
+    <div>
+    </div>
+  )
+}
