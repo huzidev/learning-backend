17ee08a84d8e5a509a79ed2e0ea99949688ccaf1
huzidev <huzaifa.iqdev@gmail.com>
2022-10-15 22:35:51 +0500

    useContext hook

--- express/todo-webpage/client/src/components/Context/TestContext.js
+++ express/todo-webpage/client/src/components/Context/TestContext.js
@@ -3,3 +3,6 @@ import { createContext } from "react";
 const TestContext = createContext();
 
 export default TestContext;
+
+// Context hooks is used so we can use any function in multiple components
+// here we've fetched user's data form mongoDB then use the data in multiple components
