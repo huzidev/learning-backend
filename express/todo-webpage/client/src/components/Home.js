d6841e190bc5b34a6e72b1ed679651d178d51f70
huzidev <huzaifa.iqdev@gmail.com>
2022-10-15 11:05:40 +0500

    errors fix

--- express/todo-webpage/client/src/components/Home.js
+++ express/todo-webpage/client/src/components/Home.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link, Navigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import {
   FileOutlined,
@@ -14,43 +14,39 @@ import PaymentOutlinedIcon from '@mui/icons-material/PaymentOutlined';
 import PaymentsOutlinedIcon from '@mui/icons-material/PaymentsOutlined';
 import KeyboardOutlinedIcon from '@mui/icons-material/KeyboardOutlined';
 import { Breadcrumb, Layout, Menu, Icon } from 'antd';
-const { Header, Content, Footer, Sider } = Layout;
-function getItem(label, key, icon, children, onClick ) {
-  return {
-    key,
-    icon,
-    children,
-    label,
-    onClick
-  };
-}
-
-
-function test() {
-  window.alert("Hello world");
-}
-
-
-
-const items = [
-  getItem('User', '1', <UserOutlined />, null, test),
-  getItem('Home', 'sub1', <HomeOutlined />, [
-    getItem('Grocery', '2', <LocalGroceryStoreOutlinedIcon />),
-    getItem('Bills', '3', <PaymentOutlinedIcon />),
-    getItem('Rent', '4', <PaymentsOutlinedIcon />)
-  ]),
-  getItem('Office', 'sub2', <MapsHomeWorkOutlinedIcon />, [
-    getItem('Project A', '5', <KeyboardOutlinedIcon /> )
-  ]),
-  getItem('Completed Tasks', '6', <FileOutlined />),
-  getItem('Contact Us', '7', <MessageOutlined />),
-  getItem('Logout', '8', <LoginOutlined />)
-];
 
 export default function App() {
   const [collapsed, setCollapsed] = useState(false);
+  const { Header, Content, Footer, Sider } = Layout;
+  function getItem(label, key, icon, children, onClick ) {
+    return {
+      key,
+      icon,
+      children,
+      label,
+      onClick
+    };
+  }
+  
   const Navigate = useNavigate();
-
+  
+  function test() {
+    Navigate('/signin')
+  }
+  const items = [
+    getItem('User', '1', <UserOutlined />),
+    getItem('Home', 'sub1', <HomeOutlined />, [
+      getItem('Grocery', '2', <LocalGroceryStoreOutlinedIcon />),
+      getItem('Bills', '3', <PaymentOutlinedIcon />),
+      getItem('Rent', '4', <PaymentsOutlinedIcon />)
+    ]),
+    getItem('Office', 'sub2', <MapsHomeWorkOutlinedIcon />, [
+      getItem('Project A', '5', <KeyboardOutlinedIcon /> )
+    ]),
+    getItem('Completed Tasks', '6', <FileOutlined />),
+    getItem('Contact Us', '7', <MessageOutlined />),
+    getItem('Logout', '8', <LoginOutlined />, null, test)
+  ];
   return (
     <Layout
       style={{
@@ -70,8 +66,6 @@ export default function App() {
           mode="inline"
           items={items}
         >
-          <Menu.Item key="8" onClick={console.log("Hello")}>
-          </Menu.Item>
         </Menu>
       </Sider>
       <Layout className="site-layout">
