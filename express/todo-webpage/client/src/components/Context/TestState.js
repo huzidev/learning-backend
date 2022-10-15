8466bd38f76dee44b6f8a0f666b824a38c235d17
huzidev <huzaifa.iqdev@gmail.com>
2022-10-15 22:18:41 +0500

    passing value as props

--- express/todo-webpage/client/src/components/Context/TestState.js
+++ express/todo-webpage/client/src/components/Context/TestState.js
@@ -2,9 +2,13 @@ import React from 'react';
 import testContext from './testContext';
 
 export default function TestState(props) {
+    const state = {
+        "name" : "Huzi"
+    }
+
   return (
     <div>
-        <TestState.Provider>
+        <TestState.Provider value={state}>
             {props.children}
         </TestState.Provider>
     </div>
