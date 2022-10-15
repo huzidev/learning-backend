c38ba18d7620a4d0cad3761a2b6d3668f60e0534
huzidev <huzaifa.iqdev@gmail.com>
2022-10-15 22:17:58 +0500

    passing props

--- express/todo-webpage/client/src/components/Context/TestState.js
+++ express/todo-webpage/client/src/components/Context/TestState.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import testContext from './testContext';
 
-export default function 
-() {
+export default function TestState(props) {
   return (
     <div>
-
+        <TestState.Provider>
+            {props.children}
+        </TestState.Provider>
     </div>
   )
 }
