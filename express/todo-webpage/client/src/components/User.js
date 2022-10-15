99215bd6ca596e0c8aedf4c6930f2d827b4f809b
huzidev <huzaifa.iqdev@gmail.com>
2022-10-15 22:34:30 +0500

    context hooks works

--- express/todo-webpage/client/src/components/User.js
+++ express/todo-webpage/client/src/components/User.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import TestContext from './Context/TestContext';
 
 export default function User() {
 
+    const context = useContext(TestContext);
+
   return (
     <div>
+        <h1>
+            UserId: {context._id}
+        </h1>
+        <h1>
+            Username: {context.username}
+        </h1>
+        <h1>
+            Email: {context.email}
+        </h1>
+        <h1>
+            Number: {context.number}
+        </h1>
     </div>
   )
 }
