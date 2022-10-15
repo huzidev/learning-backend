11d527230b9355754918a506e6444cc588e2e2a5
huzidev <huzaifa.iqdev@gmail.com>
2022-10-15 13:44:46 +0500

    ternary operators

--- express/todo-webpage/client/src/components/App.js
+++ express/todo-webpage/client/src/components/App.js
@@ -24,14 +24,11 @@ export default function App() {
                   <Home /> 
                 </React.Suspense>
               }/>
-              {localStorage.getItem('jwtoken')} ? (
-                <Route exact path="/signup" element={<SignUp />} />
-                <Route exact path="/signin" element={<SingIn />} />
-              ) : (
-                <Route exact path="/signout" element={<Logout />} />
-                <Route exact path="/about" element={<User />} />
-                <Route exact path="/contact" element={<ContactUs />} />
-              )
+              <Route exact path="/signup" element={<SignUp />} />
+              <Route exact path="/signin" element={<SingIn />} />
+              <Route exact path="/signout" element={<Logout />} />
+              <Route exact path="/about" element={<User />} />
+              <Route exact path="/contact" element={<ContactUs />} />
               <Route exact path="*" element={<Skeleton active />} />
           </Routes>
         </Router>
