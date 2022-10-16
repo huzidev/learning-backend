d5bf9ed669c3532cee444bd4a6ef3aa8f72ffc97
huzidev <huzaifa.iqdev@gmail.com>
2022-10-16 21:27:14 +0500

    category for users schema

--- express/todo-webpage/server/models/Note.js
+++ express/todo-webpage/server/models/Note.js
@@ -1,8 +1,20 @@
 import mongoose from "mongoose";
 
 const NotesSchema = new mongoose.Schema({
-    user:{
+    user: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'user'
+    },
+    tittle: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true, 
+    },
+    category: {
+        type: String,
+        default: "General"
     },
 })
