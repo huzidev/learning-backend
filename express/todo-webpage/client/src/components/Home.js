6399dac1d900eb65fefe69a6a9debac565fec8a8
huzidev <huzaifa.iqdev@gmail.com>
2022-10-16 16:38:26 +0500

    indentations

--- express/todo-webpage/client/src/components/Home.js
+++ express/todo-webpage/client/src/components/Home.js
@@ -71,7 +71,7 @@ export default function App() {
       <LoginOutlined />, 
       null, 
       () => Navigate('/signin')
-      ),
+    ),
     getItem(
       'Signup', '3', 
       <LoginOutlined />, 
