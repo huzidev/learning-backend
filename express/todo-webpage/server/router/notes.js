470b1e2f69b36b304538a70fbe423d78b9605a15
huzidev <huzaifa.iqdev@gmail.com>
2022-10-16 21:38:41 +0500

    res status

--- express/todo-webpage/server/router/notes.js
+++ express/todo-webpage/server/router/notes.js
@@ -8,6 +8,12 @@ const router = express.Router();
 router.get('/allnotes', Verification, async (req, res) => {
     try {
         const notes = await Note.find({ user: req.body.id })
+        if (notes) {
+            return res.status(200).json(notes)
+        } else {
+            return res.status(500).json({ message : "Internal server error" });
+        }
+        res.json(notes)
     } catch (e) {
         console.log(e);
     }
