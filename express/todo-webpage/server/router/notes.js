a55aae9407b58f72c7b5d8cc97ceaf66211a2fd5
huzidev <huzaifa.iqdev@gmail.com>
2022-10-16 21:33:34 +0500

    try catch

--- express/todo-webpage/server/router/notes.js
+++ express/todo-webpage/server/router/notes.js
@@ -4,3 +4,11 @@ import Note from '../models/Note';
 import { body, validationResult } from 'express-validator';
  
 const router = express.Router();
+
+router.get('/allnotes', Verification, (req, res) => {
+    try {
+        
+    } catch (e) {
+        console.log(e);
+    }
+})
