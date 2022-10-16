747050027054749ca22c8e4ed58cdc3e6a57c3b8
huzidev <huzaifa.iqdev@gmail.com>
2022-10-16 21:31:51 +0500

    express validator

--- express/todo-webpage/server/router/notes.js
+++ express/todo-webpage/server/router/notes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import Verification from '../middleware/Verification';
 import Note from '../models/Note';
-
-
+import { body, validationResult } from 'express-validator'
+ 
 const router = express.Router();
