1b5c35e86f63ed9ef3cad4ed813741d0fb954cb4
huzidev <huzaifa.iqdev@gmail.com>
2022-10-17 11:24:39 +0500

    importing useContext hook

--- express/todo-webpage/client/src/components/Todo/AddTodo.js
+++ express/todo-webpage/client/src/components/Todo/AddTodo.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, {useContext, useState} from 'react';
 
 export default function AddTodo() {
   return (
     <div>
-        
+        const context = useContext(noteContext);
     </div>
   )
 }
