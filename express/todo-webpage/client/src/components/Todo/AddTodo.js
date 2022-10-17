420e86fe545ac07da80c5c6217e5559341af25a6
huzidev <huzaifa.iqdev@gmail.com>
2022-10-17 11:26:53 +0500

    category added

--- express/todo-webpage/client/src/components/Todo/AddTodo.js
+++ express/todo-webpage/client/src/components/Todo/AddTodo.js
@@ -4,6 +4,12 @@ import DataContext from "../Context/DataContext";
 export default function AddTodo() {
     const Context = useContext(DataContext);
     const { addNote } = Context;
+
+    const [note, setNote] = useState({
+        title: "", 
+        description: "", 
+        category: ""
+    })
   return (
     <div>
     </div>
