650a34d48be3aeca3ac8033fda8a0d23173ee9f0
huzidev <huzaifa.iqdev@gmail.com>
2022-10-17 11:56:30 +0500

    button text

--- express/todo-webpage/client/src/components/Todo/AddTodo.js
+++ express/todo-webpage/client/src/components/Todo/AddTodo.js
@@ -59,7 +59,7 @@ export default function AddTodo() {
                 required 
             />
             <button onClick={handleClick}>
-
+                Add Todo
             </button>
         </form>
     </div>
