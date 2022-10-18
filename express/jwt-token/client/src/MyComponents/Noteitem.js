0cb95977273d4c14de369271f9fa38b2f8b62730
huzidev <huzaifa.iqdev@gmail.com>
2022-10-18 21:38:25 +0500

    fix

--- express/jwt-token/client/src/MyComponents/Noteitem.js
+++ express/jwt-token/client/src/MyComponents/Noteitem.js
@@ -12,8 +12,12 @@ const Noteitem = (props) => {
                 <div className="card-body">
                     <div className="d-flex align-items-center">
                         <h5 className="card-title">{note.title}</h5>
-                        <i className="far fa-trash-alt mx-2" onClick={()=>{deleteNote(note._id)}}></i>
-                        <i className="far fa-edit mx-2" onClick={()=>{updateNote(note)}}></i>
+                        <button onClick={() => deleteNote(note._id)}>
+                            Delete
+                        </button>
+                        <button onClick={() => updateNote(note)}>
+                            Update Note
+                        </button>
                     </div>
                     <p className="card-text">{note.description}</p>
 
