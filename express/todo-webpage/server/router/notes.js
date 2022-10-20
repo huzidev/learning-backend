056d429ac694a88565608a63355ad806b6783c18
huzidev <huzaifa.iqdev@gmail.com>
2022-10-20 17:15:59 +0500

    backend updated for todo'

--- express/todo-webpage/server/router/notes.js
+++ express/todo-webpage/server/router/notes.js
@@ -62,7 +62,7 @@ router.put('/updatenote/:id', Verification, async (req, res) => {
             newNote.category = category
         }
 
-        let note = await Note.findBy(req.params.id);
+        let note = await Note.findById(req.params.id);
         if (!note) {
             return res.status(404).json({ error: "Not Found" })
         }
