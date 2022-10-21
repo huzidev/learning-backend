5f493d323c5d31ab28b99e27487bd62a8156fb9e
huzidev <huzaifa.iqdev@gmail.com>
2022-10-21 21:51:53 +0500

    update users data

--- express/todo-webpage/client/src/components/User.js
+++ express/todo-webpage/client/src/components/User.js
@@ -5,6 +5,17 @@ import DataContext from './Context/DataContext';
 export default function User() {
   const context = useContext(DataContext);
   const { userData } = context
+
+  const updateData = (currData) => {
+    ref.current.click();
+    setNote({
+        id: currData._id, 
+        eusername: currData.username, 
+        eemail: currData.email, 
+        enumber:currData.number
+    })
+}
+
   return (
     <div>
         <h1>
@@ -19,9 +30,12 @@ export default function User() {
         <h1>
             Number: {userData.number}
         </h1>
-        <Link to={`/updateuser/${userData._id}`}> 
+        {/* <Link to={`/updateuser/${userData._id}`}> 
             Update Info
-        </Link>
+        </Link> */}
+        <button onClick={()=>{updateData(userData)}}>
+            Update Data
+        </button>
     </div>
   )
 }
