8055d4490b322c94feadd8de9d72ba7b55248657
huzidev <huzaifa.iqdev@gmail.com>
2022-10-24 12:25:36 +0500

    indentations

--- express/todo-webpage/client/src/components/Context/UserState.js
+++ express/todo-webpage/client/src/components/Context/UserState.js
@@ -152,7 +152,18 @@ export default function UserState(props) {
         {/* if we just use value={userData} then we simply uses context.email */}
         {/* if use value={{ userData }} multiple brackets then we've to use context.userData.email */}
         {/* {{}} multiple brackets are used when we've to pass multiple values like value={{ userData, notes }} */}
-        <DataContext.Provider value={{ userData, addNote, getNotes, editNote, deleteNote, notes, setNotes, updateUser, completedNotes, setCompletedNotes }}>
+        <DataContext.Provider value={{ 
+            userData, 
+            addNote, 
+            getNotes, 
+            editNote, 
+            deleteNote, 
+            notes, 
+            setNotes, 
+            updateUser, 
+            completedNotes, 
+            setCompletedNotes 
+        }}>
             {props.children}
         </DataContext.Provider>
     </div>
