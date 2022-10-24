08d17a1339fde080d283a2fae00bb8ac5a3da239
huzidev <huzaifa.iqdev@gmail.com>
2022-10-24 12:13:56 +0500

    completed notes schema

---
+++ express/todo-webpage/server/models/CompletedNotes.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const NotesSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'users'
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true, 
+    },
+    category: {
+        type: String,
+        default: "General"
+    },
+    date:{
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('COMPNOTES', NotesSchema);
