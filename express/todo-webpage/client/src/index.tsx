f2fd20322028d046d2e6b010f6030ce863a417e3
huzidev <huzaifa.iqdev@gmail.com>
2022-10-27 14:13:02 +0500

    react roter ts

--- express/todo-webpage/client/src/index.tsx
+++ express/todo-webpage/client/src/index.tsx
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import { Provider } from 'react-redux';
 import store from './Store';
-
+    
 ReactDOM.render(
     <Provider store={store}>
         <App />
