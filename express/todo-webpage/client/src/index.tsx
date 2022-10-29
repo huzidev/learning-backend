ad61181acbc068d11848e223d2daeb76d00bf220
huzidev <huzaifa.iqdev@gmail.com>
2022-10-29 09:56:51 +0500

    react redux provider

--- express/todo-webpage/client/src/index.tsx
+++ express/todo-webpage/client/src/index.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom';
 import App from './components/App';
-    
+import { Provider } from 'react-redux';
 ReactDOM.render(
     <App />,
     document.getElementById("root")
