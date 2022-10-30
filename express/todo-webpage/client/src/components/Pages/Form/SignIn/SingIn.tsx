40533b1574d0f1e32fe6d7074ce19e59d52f284c
huzidev <huzaifa.iqdev@gmail.com>
2022-10-30 15:46:44 +0500

    indentations

--- express/todo-webpage/client/src/components/Pages/Form/SignIn/SingIn.tsx
+++ express/todo-webpage/client/src/components/Pages/Form/SignIn/SingIn.tsx
@@ -7,41 +7,22 @@ import layout from '../../../Layout/Layout';
 import { DataType } from './Type';
 
 export default function SingIn() {
-
   const Navigate = useNavigate();
-  /* eslint-disable no-template-curly-in-string */
-  const validateMessages = {
-    required: '${label} is required!',
-    types: {
-        email: '${label} is not a valid email!',
-    },
-    password: {
-        range: '${label} must be between ${min} and ${max}',
-    },
-  };
-  /* eslint-enable no-template-curly-in-string */
-
   const [user, setUser] = React.useState<DataType>({
     email: "",
     password : ""
   });
 
-
-  let name, value;
   function inputHandler(e: React.ChangeEvent<HTMLInputElement>) {
-    name= e.target.name;
-    value= e.target.value;
     setUser({
-        ...user,
-        [name]: value
+      ...user,
+      [e.target.name]: e.target.value
     });
   };
 
   async function signIn(e: React.FormEvent) {
     e.preventDefault();
-
     const { email, password } = user;
-
     const res = await fetch(`/signin`, {
       method : "POST",
       headers : {
@@ -67,71 +48,64 @@ export default function SingIn() {
       window.location.reload();
     }
   }
-
-  const onFinish = (values: any) => {
-    console.log('Received values of form: ', values);
-  };
-
   return (
     <div>
-        <h1>
-          Singin
-        </h1>
-        <Form 
-            {...layout} 
-            name="nest-messages" 
-            onFinish={onFinish} 
-            validateMessages={validateMessages}
+      <h1>
+        Singin
+      </h1>
+      <Form 
+          {...layout} 
+          name="nest-messages" 
+      >
+        <Form.Item
+          name="email"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your Email!',
+            },
+          ]}
         >
-          <Form.Item
-            name="email"
-            rules={[
-              {
-                required: true,
-                message: 'Please input your Email!',
-              },
-            ]}
+          <Input 
+            name='email'
+            value={user.email}
+            onChange={inputHandler}
+            prefix={<UserOutlined className="site-form-item-icon" />}
+            placeholder="Email" 
+          />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your Password!',
+            },
+          ]}
+        >
+          <Input
+            type="password"
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            name='password'
+            value={user.password}
+            onChange={inputHandler}
+            placeholder="Password"
+          />
+        </Form.Item>
+        <Typography.Text>
+          Don't have an account? <Link to='/signup'>Signup</Link>
+        </Typography.Text>
+        <Form.Item>
+          <Button 
+            type="primary" 
+            htmlType="submit"
+            onClick={signIn} 
+            className="login-form-button"
           >
-            <Input 
-              name='email'
-              value={user.email}
-              onChange={inputHandler}
-              prefix={<UserOutlined className="site-form-item-icon" />}
-              placeholder="Email" 
-            />
-          </Form.Item>
-          <Form.Item
-            name="password"
-            rules={[
-              {
-                required: true,
-                message: 'Please input your Password!',
-              },
-            ]}
-          >
-            <Input
-              type="password"
-              prefix={<LockOutlined className="site-form-item-icon" />}
-              name='password'
-              value={user.password}
-              onChange={inputHandler}
-              placeholder="Password"
-            />
-          </Form.Item>
-          <Typography.Text>
-            Don't have an account? <Link to='/signup'>Signup</Link>
-          </Typography.Text>
-          <Form.Item>
-            <Button 
-              type="primary" 
-              htmlType="submit"
-              onClick={signIn} 
-              className="login-form-button"
-            >
-              Log in
-            </Button>
-          </Form.Item>
-        </Form>
+            Log in
+          </Button>
+        </Form.Item>
+      </Form>
     </div>
   )
 }
