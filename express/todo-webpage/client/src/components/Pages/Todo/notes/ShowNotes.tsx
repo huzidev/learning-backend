a15cffec2af60863d8adc5514616ee74eed51370
huzidev <huzaifa.iqdev@gmail.com>
2022-10-30 21:06:40 +0500

    ts

--- express/todo-webpage/client/src/components/Pages/Todo/notes/ShowNotes.tsx
+++ express/todo-webpage/client/src/components/Pages/Todo/notes/ShowNotes.tsx
@@ -3,7 +3,6 @@ import DataContext from "../../../Context/DataContext";
 import { Card, Col, Row , Button, Typography } from 'antd';
 
 export default function ShowNotes(props: any): JSX.Element {
-
     const context = useContext(DataContext);
     const { notes, setNotes } = context;
     const { note, updateNote, index } = props;
@@ -26,39 +25,38 @@ export default function ShowNotes(props: any): JSX.Element {
         }
       }
 
-
       const style = {
         margin: '10px 0px 0px 0px',
       }
-      return (
-          <div>
-        <Row gutter={16} style={style}>
-            <Col span={10}>
-                <Card title={
-                    <Typography.Title level={3}>
-                        Note Number : {index + 1}
-                    </Typography.Title>
-                } 
-                size="small"
-                >
-                    <Typography.Title level={4}>
-                        Title: {note.title}
-                    </Typography.Title>
-                    <Typography.Title level={5}>
-                        Description: {note.description}
-                    </Typography.Title> 
-                    <Typography.Title level={5}>
-                        Category: {note.category}
-                    </Typography.Title>
-                    <Button onClick={deleteNote}>
-                        Delete
-                    </Button>
-                    <Button onClick={() => {updateNote(note)}}>
-                        Update Note
-                    </Button>
-                </Card>
-            </Col>
-        </Row>
-    </div>
-  )
+    return (
+        <div>
+            <Row gutter={16} style={style}>
+                <Col span={10}>
+                    <Card title={
+                        <Typography.Title level={3}>
+                            Note Number : {index + 1}
+                        </Typography.Title>
+                    } 
+                    size="small"
+                    >
+                        <Typography.Title level={4}>
+                            Title: {note.title}
+                        </Typography.Title>
+                        <Typography.Title level={5}>
+                            Description: {note.description}
+                        </Typography.Title> 
+                        <Typography.Title level={5}>
+                            Category: {note.category}
+                        </Typography.Title>
+                        <Button onClick={deleteNote}>
+                            Delete
+                        </Button>
+                        <Button onClick={() => {updateNote(note)}}>
+                            Update Note
+                        </Button>
+                    </Card>
+                </Col>
+            </Row>
+        </div>
+    )
 }
