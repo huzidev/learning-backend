bd9d1be2591fa84f957dba42b7c916416c452af7
huzidev <huzaifa.iqdev@gmail.com>
2022-10-30 15:34:47 +0500

    call back function

--- express/todo-webpage/client/src/components/Pages/Form/signout/SignOut.tsx
+++ express/todo-webpage/client/src/components/Pages/Form/signout/SignOut.tsx
@@ -6,11 +6,12 @@ export default function SignOut(): JSX.Element {
     const Navigate = useNavigate();
 
     React.useEffect(() => {
-        fetch('/signout', {
+      async function test () {
+        const res = await fetch('/signout', {
           method : 'GET',
           headers : {
-              "Accept" : "application/json",
-              "Content-Type" : "application/json"
+            "Accept" : "application/json",
+            "Content-Type" : "application/json"
           },
           credentials : "include"
         }).then((res: Response) => {
@@ -30,6 +31,8 @@ export default function SignOut(): JSX.Element {
         }).catch((err: any) => {
           console.log(err);
         })
+      }
+      test()
       }, [])
 
     return (
