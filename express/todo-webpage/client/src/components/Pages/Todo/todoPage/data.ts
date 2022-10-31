6e8133adb3b9f6a9ba1eb5a39047f412e34e440d
huzidev <huzaifa.iqdev@gmail.com>
2022-10-31 16:30:45 +0500

    slides data updated

--- express/todo-webpage/client/src/components/Pages/Todo/todoPage/data.ts
+++ express/todo-webpage/client/src/components/Pages/Todo/todoPage/data.ts
@@ -1,21 +1,21 @@
 const slides = [
-    {
-      image: 'react',
-      title: 'My Todo',
-      description: 'Hello, Welcome to my Todo',
-    },
-    {
-      title: 'MERN',
-      description: 'My Todo web-page created with MongoDB, ExpressJS, React and NodeJS',
-    },
-    {
-      title: 'Logics',
-      description: "Used schema for user's private notes",
-    },
-    {
-      title: 'Security',
-      description: "Provides complete security to user's information, we uses Hash method to Hashed user's password which is one-way mechanism",
-    }
-  ];
+  {
+    image: 'react',
+    title: 'My Todo',
+    description: 'Hello, Welcome to My Todo',
+  },
+  {
+    title: 'MERN',
+    description: 'My Todo web-page created with MongoDB, ExpressJS, React and NodeJS',
+  },
+  {
+    title: 'Logics',
+    description: "Used mongoose schema for each user's private notes",
+  },
+  {
+    title: 'Security',
+    description: "Provides complete security to user's information, used Hash method to Hashed user's password which is one-way mechanism",
+  }
+];
   
   export default slides;
