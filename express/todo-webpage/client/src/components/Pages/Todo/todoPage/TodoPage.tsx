251293f0f1b5dc7e5caf4d5947e3cdcee218ee1f
huzidev <huzaifa.iqdev@gmail.com>
2022-10-31 16:14:18 +0500

    carousel added

--- express/todo-webpage/client/src/components/Pages/Todo/todoPage/TodoPage.tsx
+++ express/todo-webpage/client/src/components/Pages/Todo/todoPage/TodoPage.tsx
@@ -1,9 +1,24 @@
-import React, { useContext }from 'react';
-import { Carousel, Typography } from 'antd';
+import React, { useContext, useState }from 'react';
+import { Carousel, Typography, Col, Grid } from 'antd';
 import DataContext from '../../../Context/DataContext';
+import slides from './data'
+import './styles.less'
+
+const contentStyle: React.CSSProperties = {
+  height: '500px',
+  lineHeight: '250px',
+  textAlign: 'center',
+  background: 'rgb(0 21 41)',
+};
+
+const color: React.CSSProperties = {
+  color: 'rgb(139 173 172)',
+}
 
 export default function TodoPage(): JSX.Element {
   const context = useContext(DataContext);
+  const [activeIndex, setActiveIndex] = useState(0);
+  const breaks = Grid.useBreakpoint();
   return (
     <div>
       <h1>
@@ -17,22 +32,17 @@ export default function TodoPage(): JSX.Element {
         afterChange={(index) => setActiveIndex(index)}
       >
         {slides.map((slide, slideIndex) => (
-          <div className={cx('slide-base', slide.image)} key={slide.image}>
-            <div className={cx('slide-bg')} />
-            <Col
-              {...responsive.slider}
-              className={cx('slide-text', {
-                active: slideIndex === activeIndex,
-                mobile: !breaks.lg,
-              })}
-            >
-              <Typography.Title level={1} className={cx('title')}>
-                {slide.title}
-              </Typography.Title>
-              <Typography.Title level={4} type="secondary" className={cx('description')}>
-                {slide.description}
-              </Typography.Title>
-            </Col>
+          <div>
+            <div key={slide.title} style={contentStyle}>
+              <Col>
+                <Typography.Title level={1} style={color}>
+                  {slide.title}
+                </Typography.Title>
+                <Typography.Title level={4} type="secondary" style={color}>
+                  {slide.description}
+                </Typography.Title>
+              </Col>
+            </div>
           </div>
         ))}
       </Carousel>
---
+++ express/todo-webpage/client/src/components/Pages/Todo/todoPage/data.ts
@@ -0,0 +1,17 @@
+const slides = [
+    {
+      image: 'react',
+      title: 'Welcome',
+      description: 'Hello, My Todo',
+    },
+    {
+      title: 'MERN',
+      description: 'MyTodo web-page created with MongoDB, ExpressJS, React and NodeJS',
+    },
+    {
+      title: 'Logics',
+      description: "Used schema for different user's and for their own private notes",
+    },
+  ];
+  
+  export default slides;
