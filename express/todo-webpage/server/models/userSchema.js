69464e6d66bc1d705bfc9ae4b277a6ac536be20a
huzidev <huzaifa.iqdev@gmail.com>
2022-11-09 16:45:30 +0500

    schema for dark mode

--- express/todo-webpage/server/models/userSchema.js
+++ express/todo-webpage/server/models/userSchema.js
@@ -7,6 +7,9 @@ const userSchema = new mongoose.Schema({
         type : String,
         required : true
     },
+    isTheme: {
+        type : Boolean,
+    },
     image : {
         type : String
     },
