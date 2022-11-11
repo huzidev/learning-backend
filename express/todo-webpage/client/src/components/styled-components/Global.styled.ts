41617ad0bd0bd39c9a711c58eaa181d99c1d51d9
huzidev <huzaifa.iqdev@gmail.com>
2022-11-11 13:38:37 +0500

    const variables

--- express/todo-webpage/client/src/components/styled-components/Global.styled.ts
+++ express/todo-webpage/client/src/components/styled-components/Global.styled.ts
@@ -8,16 +8,16 @@ export const GlobalStyles = createGlobalStyle<Props>`
 
     .Dark {
         background-color: ${(p: any) => p.dark ? '#292929' : 'rgb(240 242 245 / 1)'};
-        color: white;
+        color: ${(p: any) => p.dark ? 'white' : 'black'};
         transition: all 700ms ease-in-out;
         h1, h2, h3, h4, h5, p, span, label {
-            color: white;
+            color: ${(p: any) => p.dark ? 'white' : 'black'};
             transition: all 700ms ease-in-out;
         }
         input, textarea {
             transition: all 700ms ease-in-out;
-            background-color: #292929;
-            color: white;
+            background-color: ${(p: any) => p.dark ? '#292929' : 'rgb(240 242 245 / 1)'};
+            color: ${(p: any) => p.dark ? 'white' : 'black'};
         }
     }
     /* .ant-modal-body, .ant-modal-header, .ant-modal-footer {
