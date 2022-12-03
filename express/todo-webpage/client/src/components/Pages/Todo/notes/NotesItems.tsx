984d031137116c86b37ee1f2ea3847db30f550cc
huzidev <huzaifa.iqdev@gmail.com>
2022-12-03 11:03:09 +0500

    fetching completed notes

--- express/todo-webpage/client/src/components/Pages/Todo/notes/NotesItems.tsx
+++ express/todo-webpage/client/src/components/Pages/Todo/notes/NotesItems.tsx
@@ -37,6 +37,20 @@ export default function NotesItems(props: any) {
         getNotes()
     }, [])
     
+    async function getCompNotes() {
+        const res = await fetch('/completednotes', {
+            method : 'GET',
+            headers: {
+                "Content-Type" : "application/json",
+            }
+        })
+        const data = await res.json();
+        setNotes(data)
+    }
+    useEffect(() => {
+        getCompNotes()
+    }, [])
+    
     const updateNote = (currentNote: any) => {
         // ref.current.click() checks if user clicked or not therefore we've passed the ref in the button of modal as ref={ref}
         // to check if user clicked or not
