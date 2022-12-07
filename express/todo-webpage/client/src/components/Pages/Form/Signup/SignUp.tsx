83b687e4f5d025d4fdd60ce072daab4f7d2c4829
huzidev <huzaifa.iqdev@gmail.com>
2022-12-07 12:28:30 +0500

    notificaotions icons

--- express/todo-webpage/client/src/components/Pages/Form/Signup/SignUp.tsx
+++ express/todo-webpage/client/src/components/Pages/Form/Signup/SignUp.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Button, Form, Input, Typography, notification } from 'antd';
 import { LockOutlined, UserOutlined, NumberOutlined, MailOutlined } from '@ant-design/icons';
-import layout from '../../../Layout/Layout';
+import { SmileOutlined } from '@ant-design/icons';
 import { DataType } from './Type';
 import { Link } from 'react-router-dom';
 
