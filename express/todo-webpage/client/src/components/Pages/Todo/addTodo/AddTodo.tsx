9d64b7b095b876a5e5478189d2f5a47f91a96fac
huzidev <huzaifa.iqdev@gmail.com>
2022-12-08 21:45:51 +0500

    indentations

--- express/todo-webpage/client/src/components/Pages/Todo/addTodo/AddTodo.tsx
+++ express/todo-webpage/client/src/components/Pages/Todo/addTodo/AddTodo.tsx
@@ -5,62 +5,62 @@ import { Button, Form, Input, Typography, notification } from 'antd';
 import { DataType } from './Type';
 
 export default function AddTodo(): JSX.Element {
-    const context = useContext(DataContext);
-    const {setNotes, notes} = context;
-    const [note, setNote] = useState<DataType>({
-      title: "", 
-      description: "", 
-      category: "",
-      isCompleted: false
-    })
+  const context = useContext(DataContext);
+  const {setNotes, notes} = context;
+  const [note, setNote] = useState<DataType>({
+    title: "", 
+    description: "", 
+    category: "",
+    isCompleted: false
+  })
 
-    let icon : any
-    let heading: String;
-    let message: String | null;
+  let icon : any
+  let heading: String;
+  let message: String | null;
 
-    const addTodo = async (e: React.FormEvent) => {
-      e.preventDefault();
-      const {title, description, category, isCompleted} = note
-      const res = await fetch(`/addnote`, {
-        method : "POST",
-        headers : {
-          "Content-Type" : "application/json",
-        },
-        body : JSON.stringify({
-          title,
-          description,
-          category: category.charAt(0).toUpperCase() + category.slice(1).toLowerCase(),
-          isCompleted
-        })
-      });
-      if (res.status === 404) {
-        icon = <ExclamationCircleOutlined style={{ color: '#FF0000' }}/>;
-        heading = `Empty Field`;
-        message = `You've Left A Field Empty!`;
-      }
-      else {  
-        const data = await res.json()
-        setNotes(notes.concat(data));
-        icon = <CheckCircleOutlined style={{ color: '#00FF00' }}/>;
-        heading = `Note Added`;
-        message = `Yours Note Has Been Added Successfully!`;
-      }
-      async function openNotification() {
-        notification.open({
-          icon : icon,
-          message: heading,
-          description: message
-        });
-      };
-      openNotification();
-    }
-
-    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-      setNote({
-        ...note,
-        [e.target.name]: e.target.value
+  const addTodo = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const {title, description, category, isCompleted} = note
+    const res = await fetch(`/addnote`, {
+      method : "POST",
+      headers : {
+        "Content-Type" : "application/json",
+      },
+      body : JSON.stringify({
+        title,
+        description,
+        category: category.charAt(0).toUpperCase() + category.slice(1).toLowerCase(),
+        isCompleted
       })
+    });
+    if (res.status === 404) {
+      icon = <ExclamationCircleOutlined style={{ color: '#FF0000' }}/>;
+      heading = `Empty Field`;
+      message = `You've Left A Field Empty!`;
     }
+    else {  
+      const data = await res.json()
+      setNotes(notes.concat(data));
+      icon = <CheckCircleOutlined style={{ color: '#00FF00' }}/>;
+      heading = `Note Added`;
+      message = `Yours Note Has Been Added Successfully!`;
+    }
+    async function openNotification() {
+      notification.open({
+        icon : icon,
+        message: heading,
+        description: message
+      });
+    };
+    openNotification();
+  }
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNote({
+      ...note,
+      [e.target.name]: e.target.value
+    })
+  }
 
   return (
     <div style={{ width: '650px', margin: 'auto' }}>
