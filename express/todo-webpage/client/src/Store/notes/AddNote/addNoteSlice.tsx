3dcf7a1fd2812fe4d68cfa6fb71f5ed2827f342b
huzidev <huzaifa.iqdev@gmail.com>
2022-12-27 21:21:27 +0500

    user add note

---
+++ express/todo-webpage/client/src/Store/notes/AddNote/addNoteSlice.tsx
@@ -0,0 +1,39 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { InitialType } from "./Types";
+
+const initialState: InitialType = {
+    res: null
+}
+
+export const addThisNote = createAsyncThunk('user/addnote', async (user: any) => {
+    const { username, email, number, message } = user;
+    console.log("user info", user);
+    const res = await fetch("/contact", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            username,
+            email,
+            number: parseInt(number),
+            message
+        })
+    });
+    const data = await res.json();
+    return data
+})
+
+const contactSlice = createSlice({
+    name: 'contact',
+    initialState,
+    reducers: {
+        receiveTEst(state, action) {
+            state.res = action.payload
+        }
+    },
+})
+
+export default contactSlice.reducer
+
+export const contactAction = contactSlice.actions;
