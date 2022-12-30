ca417f960b4745f2d6c8772b9956664803b019a2
huzidev <huzaifa.iqdev@gmail.com>
2022-12-30 22:12:05 +0500

    updateNote redux

--- express/todo-webpage/client/src/Store/notes/UpdateNote/updateSlice.tsx
+++ express/todo-webpage/client/src/Store/notes/UpdateNote/updateSlice.tsx
@@ -1,9 +1,9 @@
 import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
 
-export const updateThisNote = createAsyncThunk('user/notes', async (note: any) => {
+export const updateThisNote = createAsyncThunk('user/updateNote', async (note: any) => {
     try {
-        const { htitle, hdescription, hcategory, hIsCompleted } = note
-        const res = await fetch(`/updatenote/${id}`, {
+        const { hid, htitle, hdescription, hcategory, hIsCompleted } = note
+        const res = await fetch(`/updatenote/${hid}`, {
             method: 'PUT',
             headers: new Headers({
                 "Content-Type": "application/json",
@@ -19,13 +19,3 @@ export const updateThisNote = createAsyncThunk('user/notes', async (note: any) =
         console.log(err);
     }
 })
-
-const noteSlice = createSlice({
-    name: 'note',
-    initialState,
-    reducers: {},
-})
-
-export default noteSlice.reducer
-
-export const noteAction = noteSlice.actions;
