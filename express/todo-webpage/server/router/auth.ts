7f380d7f5153cb632152d1abbba4c4469f75624a
huzidev <huzaifa.iqdev@gmail.com>
2022-12-31 14:03:57 +0500

    backend updated

--- express/todo-webpage/server/router/auth.ts
+++ express/todo-webpage/server/router/auth.ts
@@ -34,18 +34,18 @@ router.post('/signup', async (req: any, res: Response) => {
         const usernameExist = await User.findOne({ username : username });
         const numberExist = await User.findOne({ number : number });
 
-        if (emailExist) {
-            return res.status(422).json({ error: "Email already Exist" });
-        } else if (usernameExist) {
-            return res.status(423).json({ error: "Username already Exist" });
+        if (usernameExist) {
+            return res.status(422).json({ error: "Username already Exist" });
+        } else if (emailExist) {
+            return res.status(423).json({ error: "Email already Exist" });
         } else if (numberExist) {
             return res.status(424).json({ error: "Number already Exist" });
         } else if (password !== cpassword) {
             return res.status(425).json({ error : "Password doesn't match" });
         } else if (username.length < 3) {
-            return res.status(426).json({ error : "Username's length must be greater than 2 characters" });
+            return res.status(426).json({ error : "Username must be 3 characters Long" });
         } else if (password.length < 6 || cpassword.length < 6) {
-            return res.status(427).json({ error : "Password's length must be greater than 6 characters" });
+            return res.status(427).json({ error : "Password must be 6 characters Long" });
         } 
         // when user registered successfully
         else {
