9f719ebab4e6bb32ded939bdc0fe97418ee71d04
huzidev <huzaifa.iqdev@gmail.com>
2022-12-31 14:31:49 +0500

    running test

--- express/todo-webpage/client/src/components/Pages/Form/Signup/SignUp.tsx
+++ express/todo-webpage/client/src/components/Pages/Form/Signup/SignUp.tsx
@@ -56,19 +56,24 @@ export default function SignUp() {
   console.log("Res From TypeScript Code", res);
 
   const { username, email, number, password, cpassword, isTheme } = user;
-    if ((username || email || number || password || cpassword) === "") {
-        dispatch(signupAction.receiveTEst(421));
-        field = username === "" ? 
-            "Username" : email === "" ? 
-            "Email" : number === "" ? 
-            "Number" : password === "" ? 
-            "Password" : cpassword === "" ? 
-            "Confirm Password" : "";
-    } else if (password !== cpassword) {
-        dispatch(signupAction.receiveTEst(425));
-    } else if (username.length < 3) {
-        dispatch(signupAction.receiveTEst(426));
-    } 
+    // if (username || email || number || password || cpassword === "") {
+    //     dispatch(signupAction.receiveTEst(421));
+    //     field = username === "" ? 
+    //         "Username": email === "" ? 
+    //         "Email" : number === "" ? 
+    //         "Number" : password === "" ? 
+    //         "Password" : cpassword === "" ? 
+    //         "Confirm Password" : "";
+    // } 
+    if (username === "") {
+            dispatch(signupAction.receiveTEst(421));
+    }
+    // else if (password !== cpassword) {
+    //     dispatch(signupAction.receiveTEst(425));
+    // } 
+    // else if (username.length < 3) {
+    //     dispatch(signupAction.receiveTEst(426));
+    // } 
     // else if ((password || cpassword) !== "" && (password.length || cpassword.length) < 6) {
     //     dispatch(signupAction.receiveTEst(427));
     // }
