b2478e83f5f4410785dd178e98f676d809b20454
huzidev <huzaifa.iqdev@gmail.com>
2023-02-02 14:56:03 +0500

    react components

--- express/todo-webpage/client/src/components/Pages/Todo/notes/NoteState.tsx
+++ express/todo-webpage/client/src/components/Pages/Todo/notes/NoteState.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import { Typography } from 'antd';
 
 export default function NoteState(props: any) {
   return (
     <div>
+        <Typography.Title level={4}>
+            No Task {props.type}
+        </Typography.Title>
 
     </div>
   )
