345d785fa119b8f78b41e3daf1c700735e16f2cc
huzidev <huzaifa.iqdev@gmail.com>
2023-02-04 11:15:12 +0500

    result type antd

--- express/todo-webpage/client/src/components/Error.tsx
+++ express/todo-webpage/client/src/components/Error.tsx
@@ -1,9 +1,30 @@
+import { Card, Result } from "antd"
 export default function Error(): JSX.Element {
   return (
     <div>
-        <h1>
-            404! Error Not Found
-        </h1>
+      <Card>
+        <Result
+          // status 403 and status 404 have different images according to the status
+          status="403"
+          title="403"
+          subTitle={`The apartment you tries to access might be Rented would you like to check more apartment from the user who've uploaded that apartment`}
+          extra={
+            <>
+              <Button
+                type="primary"
+                onClick={() =>
+                  history.push(`${ROUTE_PATHS.VIEW_PROFILE}${state.data?.realtorId}`)
+                }
+              >
+                Check More
+              </Button>
+              <Button type="primary" onClick={() => history.replace(ROUTE_PATHS.DASHBOARD)}>
+                Back Home
+              </Button>
+            </>
+          }
+        />
+      </Card>
     </div>
   )
 }
