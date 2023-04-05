7206180144a6f53f4c7d7be88169a16abdbcf2f4
huzidev <huzaifa.iqdev@gmail.com>
2023-04-05 20:29:15 +0500

    mysql-web

---
+++ mySQL/mysql-web/server/index.js
@@ -0,0 +1,76 @@
+import cors from "cors";
+import express from "express";
+import mysql from "mysql";
+
+const server = express();
+
+server.use(cors());
+server.use(express.json());
+
+const db = mysql.createConnection({
+    user: "root",
+    host: "localhost",
+    password: "password",
+    database: "employeeSystem",
+});
+
+server.post("/create", (req, res) => {
+    const name = req.body.name;
+    const age = req.body.age;
+    const country = req.body.country;
+    const position = req.body.position;
+    const wage = req.body.wage;
+
+    db.query(
+        "INSERT INTO employees (name, age, country, position, wage) VALUES (?,?,?,?,?)",
+        [name, age, country, position, wage],
+        (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.send("Values Inserted");
+            }
+        }
+    );
+});
+
+server.get("/employees", (req, res) => {
+    db.query("SELECT * FROM employees", (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(result);
+        }
+    });
+});
+
+server.put("/update", (req, res) => {
+    const id = req.body.id;
+    const wage = req.body.wage;
+    db.query(
+        "UPDATE employees SET wage = ? WHERE id = ?",
+        [wage, id],
+        (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.send(result);
+            }
+        }
+    );
+});
+
+server.delete("/delete/:id", (req, res) => {
+    const id = req.params.id;
+    db.query("DELETE FROM employees WHERE id = ?", id, (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(result);
+        }
+    });
+});
+
+server.listen(3001, () => {
+    console.log("Yey, your server is running on port 3001");
+});
